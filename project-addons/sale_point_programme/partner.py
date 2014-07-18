3f6560bb8108ccba8b82bf10a29eb10b3b3425db
omar7r <omarcs7r@gmail.com>
2014-07-18 17:20:06 +0200

    [ADD] sale_point_programme: Primera versión en desarrollo del módulo de programa de puntos

---
+++ project-addons/sale_point_programme/partner.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Copyright (C) 2014 Pexego Sistemas Informáticos All Rights Reserved
+#    $Omar Castiñeira Saavedra <omar@pexego.es>$
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published
+#    by the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+from openerp import models, fields, api
+
+
+class ResPartner(models.Model):
+
+    _inherit = "res.partner"
+
+    points_in_bag = fields.Integer(compute='_get_points', string='Points',
+                                   readonly=True)
+
+    @api.one
+    def _get_points(self):
+        self.points_in_bag = \
+            sum([x.points for x in
+                 self.env['res.partner.point.programme.bag'].
+                 search([('partner_id', '=', self.id)])])
