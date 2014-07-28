aa30a3424e2bfd3d4c9b21e6e28110acad9be32a
jesusVMayor <jesus@pexego.es>
2014-07-28 09:52:47 +0200

    [ADD]Añadidos módulos.
    Añadidos los modulos nan_partner_risk y partner_risk__stock_reserve__rel.

---
+++ project-addons/nan_partner_risk/__openerp__.py
@@ -0,0 +1,28 @@
+# -*- encoding: utf-8 -*-
+{
+    "name" : "Partner Risk Analysis",
+    "version" : "0.1",
+    "description" : """This module adds a new button in the partner form to analyze current state of a partner risk.
+It reports current information regarding amount of debt in invoices, orders, etc.
+
+It also modifies the workflow of sale orders by adding a new step when partner's risk is exceeded.
+
+Developed for Trod y Avia, S.L.""",
+    "author" : "NaN·tic",
+    "website" : "http://www.NaN-tic.com",
+    "depends" : [
+        'base',
+        'account',
+        'sale',
+        'sale_stock',
+        'account_payment',
+    ],
+    "category" : "Custom Modules",
+    "data" : [
+        'wizard/open_risk_window_view.xml',
+        'risk_view.xml',
+        'sale_view.xml',
+        'sale_workflow.xml'
+    ],
+    "installable": True
+}
---
+++ project-addons/partner_risk__stock_reserve__rel/__openerp__.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Copyright (C) 2014 Pexego Sistemas Informáticos All Rights Reserved
+#    $Jesús Ventosinos Mayor <jesus@pexego.es>$
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published
+#    by the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+{
+    'name': "Relation betweend partner_risk and sale_reserve module",
+    'version': '1.0',
+    'category': 'sale',
+    'description': """Add a relationship between partner_risk and sale_reserve to the sales workflow is not overwritted""",
+    'author': 'Pexego Sistemas Informáticos',
+    'website': 'www.pexego.es',
+    "depends": ['sale',
+                'sale_stock',
+                'nan_partner_risk',
+                'stock_reserve_sale'],
+    "data": ['sale_workflow.xml'],
+    "installable": True
+}
