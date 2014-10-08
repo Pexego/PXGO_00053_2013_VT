174cf27161fb4a0e1e975b70ce1899d8ba58bb53
jesusVMayor <jesus@pexego.es>
2014-10-08 13:42:48 +0200

    [ADD]purchase_picking: al crear un albaran desde movimientos se puede modificar la cantidad.

--- project-addons/purchase_picking/wizard/create_picking_move.py
+++ project-addons/purchase_picking/wizard/create_picking_move.py
@@ -20,22 +20,44 @@
 ##############################################################################
 
 
-from openerp import models, fields, api
+from openerp import models, fields, api, exceptions, _
+
+class move_details(models.TransientModel):
+
+    _name = 'picking.wizard.move.details'
+
+    product_id = fields.Many2one('product.product', 'Product')
+    qty = fields.Float('Quantity')
+    move_id = fields.Many2one('stock.move', 'Move')
+    wizard_id = fields.Many2one('picking.from.moves.wizard', 'wizard')
 
 
 class create_picking_move(models.TransientModel):
 
+    @api.model
+    def _get_lines(self):
+        wiz_lines = []
+        move_ids = self.env.context.get('active_ids', [])
+        for move in self.env['stock.move'].browse(move_ids):
+            if move.state != u'draft' or move.picking_id:
+                continue
+            wiz_lines.append({'product_id': move.product_id.id,
+                              'qty': move.product_uom_qty,
+                              'move_id': move.id})
+        return wiz_lines
+
     _name = "picking.from.moves.wizard"
 
     date_picking = fields.Datetime('Date planned', required=True)
-
+    move_detail_ids = fields.One2many('picking.wizard.move.details',
+                                      'wizard_id', 'lines', default=_get_lines)
 
     def _view_picking(self):
         action = self.env.ref('stock.action_picking_tree').read()[0]
         pick_ids = self.env.context.get('picking_ids', [])
-        #override the context to get rid of the default filtering on picking type
+        # override the context to get rid of the default filtering on picking type
         action['context'] = {}
-        #choose the view_mode accordingly
+        # choose the view_mode accordingly
         if len(pick_ids) > 1:
             action['domain'] = "[('id','in',[" + ','.join(map(str, pick_ids)) + "])]"
         else:
@@ -49,23 +71,29 @@ class create_picking_move(models.TransientModel):
         context = self.env.context
         if not context.get('active_ids', False):
             return
-        moves = self.env['stock.move'].browse(context['active_ids'])
+
         picking_types = {}
         all_moves = []
-        partner_id = moves[0].partner_id.id
+        partner_id = self.move_detail_ids[0].move_id.partner_id.id
         same_partner = True
-
         # se recorren los movimientos para agruparlos por tipo
-        for move in moves:
-            if move.state != u'draft' or move.picking_id:
-                continue
-            if partner_id != move.partner_id.id:
+        for move in self.move_detail_ids:
+
+            if partner_id != move.move_id.partner_id.id:
                 same_partner = False
-            if move.picking_type_id.id not in picking_types.keys():
-                picking_types[move.picking_type_id.id] = []
-            picking_types[move.picking_type_id.id].append(move)
-            move.date_expected = self.date_picking
-            all_moves.append(move.id)
+            if move.move_id.picking_type_id.id not in picking_types.keys():
+                picking_types[move.move_id.picking_type_id.id] = []
+            if move.qty != move.move_id.product_uom_qty:
+                if move.qty > move.move_id.product_uom_qty:
+                    raise exceptions.except_orm(_('Quantity error'), _('The quantity is greater than the original.'))
+                new_move = move.move_id.copy({'product_uom_qty': move.qty})
+                new_move.purchase_line_id = move.move_id.purchase_line_id
+                picking_types[move.move_id.picking_type_id.id].append(new_move)
+                move.move_id.product_uom_qty = move.move_id.product_uom_qty - move.qty
+            else:
+                picking_types[move.move_id.picking_type_id.id].append(move.move_id)
+                move.move_id.date_expected = self.date_picking
+                all_moves.append(move.move_id.id)
         if not same_partner:
             partner_id = self.env.ref('purchase_picking.partner_multisupplier').id
         picking_ids = []
