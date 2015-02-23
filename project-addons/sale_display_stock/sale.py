203c31e33908cac7efc41de545be3da53eaf682a
jesusVMayor <jesus@pexego.es>
2015-02-23 11:34:34 +0100

    [MOD]revertidos cambios de https://github.com/Pexego/PXGO_00053_2013_VT/commit/4904cbbde4c5dc97a6e2c7329a7e159f13f8ba1b

--- project-addons/sale_display_stock/sale.py
+++ project-addons/sale_display_stock/sale.py
@@ -27,22 +27,7 @@ class sale_order_line(models.Model):
     _inherit = 'sale.order.line'
 
     qty_available = fields.Float('Qty available', readonly=True,
+                                  related='product_id.qty_available',
                                   digits_compute=
                                   dp.get_precision('Product \
                                                     Unit of Measure'))
-
-    def product_id_change(self, cr, uid, ids, pricelist, product, qty=0,
-                          uom=False, qty_uos=0, uos=False, name='',
-                          partner_id=False, lang=False, update_tax=True,
-                          date_order=False, packaging=False,
-                          fiscal_position=False, flag=False, context=None):
-        res = super(sale_order_line, self).product_id_change(
-            cr, uid, ids, pricelist, product, qty, uom, qty_uos, uos, name,
-            partner_id, lang, update_tax, date_order, packaging,
-            fiscal_position, flag, context)
-        if product:
-            product_obj = self.pool.get('product.product').browse(cr, uid,
-                                                                  product,
-                                                                  context)
-            res['value']['qty_available'] = product_obj.qty_available
-        return res
