a480b6e7b73820898d206c80fd99e786846d0e3c
Omar <omarcs7r@gmail.com>
2015-06-04 10:48:21 +0200

    [IMP] sale_product_customize: Se crea un asistente para crear produccione sunitarias dada una cantidad de producto y un producto final

--- project-addons/sale_product_customize/wizard/mrp_customization_wizard.py
+++ project-addons/sale_product_customize/wizard/mrp_customization_wizard.py
@@ -26,14 +26,27 @@ class MrpCustomizationWizard(models.TransientModel):
 
     _name = "mrp.customization.wizard"
 
-    product_id = fields.Many2one('product.product', 'Product')
-    qty = fields.Float('Quantity')
-    customization_type = fields.Many2one('mrp.customize.type', 'Type')
-    product_uom = fields.Many2one('product.uom', 'UoM')
+    product_id = fields.Many2one('product.product', 'Product', required=True,
+                                 domain=[('type', '=', 'product')])
+    qty = fields.Float('Quantity', required=True)
+    customization_type_ids = fields.Many2many('mrp.customize.type',
+                                              'customization_wzd_ctype_rel',
+                                              'wzd_id', 'ctype_id',
+                                              'Customization types')
+    product_uom = fields.Many2one('product.uom', 'UoM', readonly=True)
+
+    @api.one
+    @api.onchange('product_id')
+    def on_change_product_id(self):
+        if self.product_id:
+            self.product_uom = self.product_id.uom_id.id
+        else:
+            self.product_uom = False
 
     @api.one
     def create_customization(self):
-        bom = self.env['mrp.bom'].search([('product_id', '=', self.product_id.id)])
+        bom = self.env['mrp.bom'].search([('product_id', '=',
+                                           self.product_id.id)])
         bom_id = bom and bom[0].id or False
         if not bom_id:
             bom_list_dict = {
@@ -41,20 +54,24 @@ class MrpCustomizationWizard(models.TransientModel):
                 'product_tmpl_id': self.product_id.product_tmpl_id.id,
                 'product_id': self.product_id.id,
                 'bom_line_ids':
-                    [(0, 0,
-                      {'product_id': self.product_id.id,
-                       'product_qty': 1,
-                       'final_lot': True})],
+                [(0, 0, {'product_id': self.product_id.id,
+                         'product_qty': 1,
+                         'final_lot': True})]
             }
             bom_id = self.env['mrp.bom'].create(bom_list_dict).id
-        mrp_args = {
-            'type_id': self.customization_type.id,
-            'product_id': self.product_id.id,
-            'bom_id': bom_id,
-            'product_uom': self.product_uom.id,
-            'product_qty': self.qty,
-        }
-        production = self.env['mrp.production'].create(mrp_args)
-        production.signal_workflow('button_confirm')
+        if self.customization_type_ids:
+            type_ids = [(6, 0, [x.id for x in self.customization_type_ids])]
+        else:
+            type_ids = False
+        for x in range(int(self.qty)):
+            mrp_args = {
+                'type_ids': type_ids,
+                'product_id': self.product_id.id,
+                'bom_id': bom_id,
+                'product_uom': self.product_id.uom_id.id,
+                'product_qty': 1
+            }
+            production = self.env['mrp.production'].create(mrp_args)
+            production.signal_workflow('button_confirm')
 
         return True
