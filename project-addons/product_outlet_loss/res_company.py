566d174283b49a923d0a161e1887c93abc3454ca
Kiko <kiko@comunitea.com>
2015-07-14 13:46:30 +0200

    [FIX] Se corrige código

---
+++ project-addons/product_outlet_loss/res_company.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Copyright (C) 2014 Comunitea Servicios Tecnológicos All Rights Reserved
+#    $Kiko Sánchez <kiko@comunitea.com>$
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published
+#    by the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+from openerp import fields, models, api
+from openerp.exceptions import ValidationError
+
+
+class res_company(models.Model):
+    _inherit = "res.company"
+
+    outlet_per_cent = fields.Float("% Outlet Price")
+
+    @api.constrains('outlet_per_cent')
+    def _check_outlet_per_cent(self):
+        if self.outlet_per_cent < 0:
+            raise ValidationError("% must be > 0")
+        if self.outlet_per_cent >= 100:
+            raise ValidationError("% must be <=100")
