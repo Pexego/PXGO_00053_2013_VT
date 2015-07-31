02e92201f159708e1781047d49582b05ccae1660
Kiko <kiko@comunitea.com>
2015-07-31 15:39:12 +0200

    [ADD] partner_risk_advice: Envío de notificaciones por rotura de riesgo

---
+++ project-addons/partner_risk_advice/res_partner.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Copyright (C) 2014 Comunitea Servicios Tecnológicos All Rights Reserved
+#    $Kiko Sánchez <kiko@comunitea.com>$
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published
+#    by the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+from openerp import fields, models, api
+from openerp.exceptions import ValidationError
+
+
+class res_partner(models.Model):
+    _inherit = "res.partner"
+
+    risk_advice_ids = fields.One2many ("partner.risk.advice", "partner_id")
+
+    # Descomentar para probar el cron con el botón en el formulario partner
+    #@api.multi
+    #def check_partner_risk(self):
+    #    e = self.env['partner.risk.advice'].check_partner_risk()
---
+++ project-addons/partner_risk_advice/risk_advice.py
@@ -0,0 +1,129 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Copyright (C) 2015 Comunitea Servicios Tecnológicos All Rights Reserved
+#    $Kiko Sánchez <kiko@comunitea.com>$
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published
+#    by the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+
+
+from openerp import fields, models, api, _
+import time
+from datetime import datetime
+from dateutil import relativedelta
+
+class RiskAdviceMail(models.Model):
+
+    _name = "partner.risk.advice"
+
+    days_after= fields.Integer("Days from last invoice")
+    global_ok = fields.Boolean ("Global", default = False, help = "True: all partners but those with specific advices")
+    partner_id = fields.Many2one("res.partner", "Customer")
+    template_id = fields.Many2one("email.template", "Template", required = True)
+
+    _sql_constraints = [
+        ('name_uniq', 'unique(global_ok, days_after, partner_id)', 'global_ok, days_after must be unique')
+    ]
+
+    @api.model
+    def check_partner_risk(self):
+
+        partner_pool = self.env['res.partner'].search([])
+
+        for partner in partner_pool:
+            accounts = []
+            res = False
+            break_risk=False
+
+            if partner.property_account_receivable:
+                accounts.append( partner.property_account_receivable.id )
+
+            if partner.property_account_payable:
+                accounts.append( partner.property_account_payable.id )
+
+            line_ids = self.env['account.move.line'].search([
+                ('partner_id','=',partner.id),
+                ('account_id', 'in', accounts),
+                ('reconcile_id','=',False)
+                ], order = "date_maturity asc")
+
+            amount = 0.0
+
+            for line in line_ids:
+
+                if line.currency_id:
+                    sign = line.amount_currency < 0 and -1 or 1
+                else:
+                    sign = (line.debit - line.credit) < 0 and -1 or 1
+                amount += sign * line.amount_residual
+
+                if amount > partner.available_risk:
+                    #rotura de risk
+                    res = {
+                        'partner': partner.id,
+                        'line': line.id,
+                        'amount': amount - partner.available_risk,
+                        'date' : line.date_maturity,
+                        'name' : line.ref
+                    }
+                    ok  = self.send_risk_advice_mail (res)
+                    break_risk= True
+                    break #line in line in line_ids
+
+            if break_risk == True:
+                break #partner in partner_pool
+
+        return res
+
+
+    def send_risk_advice_mail (self, values):
+
+        partner= self.env['res.partner'].search([('id', '=', values['partner'])])
+        mail_pool = self.env['mail.mail']
+        mail_ids = self.env['mail.mail']
+        date = datetime.strptime(str(values['date']), '%Y-%m-%d')
+        today = datetime.strptime(str(fields.Date.today()), '%Y-%m-%d')
+        timing = relativedelta.relativedelta(today, date)
+
+        if partner.risk_advice_ids:
+            advice_pool = partner.risk_advice_ids
+        else:
+            advice_pool = self.env['partner.risk.advice'].search([('global_ok','=',True)])
+
+        for advice in advice_pool:
+
+            if timing.days == advice.days_after:
+                template_id = advice.template_id
+                advice_id = {'advice' : advice.id}
+
+                if not template_id:
+                    break
+
+                ctx = dict(self._context)
+                ctx.update({
+                    'partner_email': partner.email,
+                    'partner_id': partner.id,
+                    'partner_name': partner.name,
+                    'mail_from': self.env.user.company_id.email,
+                })
+                ctx.update (values)
+                ctx.update (advice_id)
+
+                mail_id = template_id.with_context(ctx).send_mail(partner.id)
+                mail_ids += mail_pool.browse(mail_id)
+                if mail_ids:
+                    mail_ids.send()
