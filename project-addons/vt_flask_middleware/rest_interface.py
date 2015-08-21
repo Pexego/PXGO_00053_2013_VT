61ed2eeb442e397c372268f1126e27e9c4c075f9
Omar <omarcs7r@gmail.com>
2015-08-21 14:48:35 +0200

    [ADD] vt_flask_middleware: Se añade autenticación al api rest del Flask y se restringen todas las formas de conexión excepto GET

--- project-addons/vt_flask_middleware/rest_interface.py
+++ project-addons/vt_flask_middleware/rest_interface.py
@@ -1,12 +1,24 @@
-from flask_peewee.rest import RestAPI, RestResource
+from flask_peewee.rest import RestAPI, RestResource, UserAuthentication
 
 from app import app
 from customer import Customer
 from product import Product
+from auth import auth
 
-api = RestAPI(app)
+user_auth = UserAuthentication(auth, protected_methods=['GET', 'POST', 'PUT', 'DELETE'])
+api = RestAPI(app, default_auth=user_auth)
 
-api.register(Customer)
-api.register(Product)
+class ApiResource(RestResource):
+    def check_post(self, obj=None):
+        return False
+
+    def check_put(self, obj):
+        return False
+
+    def check_delete(self, obj):
+        return False
+
+api.register(Customer, ApiResource)
+api.register(Product, ApiResource)
 
 api.setup()
