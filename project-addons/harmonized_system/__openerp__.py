d3548df19aa3052aa7cb5061efab86c4332ec96a
Omar <omarcs7r@gmail.com>
2016-02-05 15:49:54 +0100

    [IMP] harmonized_system: Reglas de seguridad, pequeñas correcciones

--- project-addons/harmonized_system/__openerp__.py
+++ project-addons/harmonized_system/__openerp__.py
@@ -26,7 +26,8 @@
     'description': """""",
     'author': 'Comunitea',
     'website': 'www.comunitea.com',
-    "depends": ['base','product','purchase'],
-    "data": ['hs_codes_view.xml'],
+    "depends": ['base', 'product', 'purchase', 'account', 'stock'],
+    "data": ['hs_codes_view.xml',
+             'security/ir.model.access.csv'],
     "installable": True
 }
