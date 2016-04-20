4ffd07417bc9c6530a30ba7105ee9c7d2d0dc4e9
Omar <omarcs7r@gmail.com>
2016-04-20 14:44:59 +0200

    [REF] sale_product_customize: Pequeña refactortización para poder actualizar el precio de los productos montados una vez creados

--- project-addons/sale_product_customize/model/product.py
+++ project-addons/sale_product_customize/model/product.py
@@ -49,6 +49,24 @@ class ProductProduct(models.Model):
         'head_product_id',
         'Can mount')
 
+    def _update_product_prices(self, first_prod, sec_prod, product_mount):
+        prod_dict = {}
+        prod_dict['standard_price'] = first_prod.standard_price + \
+            (sec_prod.standard_price * product_mount.qty)
+        prod_dict['list_price'] = first_prod.list_price + \
+            (sec_prod.list_price * product_mount.qty)
+        prod_dict['list_price2'] = first_prod.list_price2 + \
+            (sec_prod.list_price2 * product_mount.qty)
+        prod_dict['list_price3'] = first_prod.list_price3 + \
+            (sec_prod.list_price3 * product_mount.qty)
+        prod_dict['pvi1_price'] = first_prod.pvi1_price + \
+            (sec_prod.pvi1_price * product_mount.qty)
+        prod_dict['pvi2_price'] = first_prod.pvi2_price + \
+            (sec_prod.pvi2_price * product_mount.qty)
+        prod_dict['pvi3_price'] = first_prod.pvi3_price + \
+            (sec_prod.pvi3_price * product_mount.qty)
+        return prod_dict
+
     def get_product_customized(self, prod_code, product_mount):
         product = self.search([('default_code', '=', prod_code)])
         if not product:
@@ -75,25 +93,9 @@ class ProductProduct(models.Model):
                 else:
                     sec_prod = self.search([('default_code', '=', code[1])])
                 prod_dict['name'] = first_prod.name + u' - ' + sec_prod.name
-                prod_dict['standard_price'] = first_prod.standard_price + \
-                    (sec_prod.standard_price * product_mount.qty)
-                prod_dict['list_price'] = first_prod.list_price + \
-                    (sec_prod.list_price * product_mount.qty)
-                prod_dict['list_price2'] = first_prod.list_price2 + \
-                    (sec_prod.list_price2 * product_mount.qty)
-                prod_dict['list_price3'] = first_prod.list_price3 + \
-                    (sec_prod.list_price3 * product_mount.qty)
-                prod_dict['pvi1_price'] = first_prod.pvi1_price + \
-                    (sec_prod.pvi1_price * product_mount.qty)
-                prod_dict['pvi2_price'] = first_prod.pvi2_price + \
-                    (sec_prod.pvi2_price * product_mount.qty)
-                prod_dict['pvi3_price'] = first_prod.pvi3_price + \
-                    (sec_prod.pvi3_price * product_mount.qty)
-                om_lines = \
-                    [(0, 0,
-                      {'product_id': first_prod.id,
-                       'product_qty': 1,
-                       'final_lot': True})]
+                prod_dict.update(self._update_product_prices(first_prod,
+                                                             sec_prod,
+                                                             product_mount))
                 qty = product_mount.qty
                 while qty > 0:
                     bom_lines.append((0, 0,
@@ -122,5 +124,18 @@ class ProductProduct(models.Model):
             }
             self.env['mrp.bom'].create(bom_list_dict)
         else:
-            product.custom = True
+            prod_dict = {'custom': True}
+            if '#' in prod_code:
+                # hay varios productos
+                code = prod_code.split('#')
+                first_prod = self.search([('default_code', '=', code[0])])
+                if '|' in code[1]:
+                    sec_prod = self.search([('default_code', '=',
+                                             code[1][:code[1].index('|')])])
+                else:
+                    sec_prod = self.search([('default_code', '=', code[1])])
+                prod_dict.update(self._update_product_prices(first_prod,
+                                                             sec_prod,
+                                                             product_mount))
+            product.write(prod_dict)
         return product
