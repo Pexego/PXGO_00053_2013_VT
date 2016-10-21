be43f912050e797ff03ca80fef59a82b6c180ccc
Omar <omarcs7r@gmail.com>
2016-10-21 15:17:39 +0200

    [ADD] product_versions: Nuevo móudlo que permite representar versiones de productos

---
+++ project-addons/product_versions/__openerp__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# © 2016 Comunitea
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Product versions",
+    "summary": "",
+    "version": "8.0.1.0.0",
+    "category": "Products",
+    "website": "http://www.comunitea.com",
+    "author": "Comunitea",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": [
+        "product",
+    ],
+    "data": [
+        'views/product_view.xml',
+        'security/ir.model.access.csv'
+    ],
+}
---
+++ project-addons/product_versions/models/product.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# © 2016 Comunitea
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from openerp import models, fields
+
+
+class ProductVersion(models.Model):
+
+    _name = "product.version"
+
+    name = fields.Text("Description", required=True)
+    version = fields.Char("Version", required=True, size=64)
+    product_tmpl_id = fields.Many2one("product.template", "Product",
+                                      required=True)
+
+
+class ProductTemplate(models.Model):
+
+    _inherit = "product.template"
+
+    version_ids = fields.One2many("product.version", "product_tmpl_id",
+                                  "Versions")
