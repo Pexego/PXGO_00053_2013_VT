9dd5e1a1b4a6414d4be75686d6966d8b60f195fb
jgmanzanas <jgmanzanas@visiotech.es>
2016-11-15 11:16:34 +0100

    Añadido total de productos de albaran a los albaranes de entrada y de salida

--- project-addons/picking_document/stock.py
+++ project-addons/picking_document/stock.py
@@ -19,12 +19,13 @@
 #
 ##############################################################################
 
-from openerp import fields, models
-
+from openerp import fields, api, models, _
+import openerp.addons.decimal_precision as dp
 
 class stock_picking(models.Model):
 
     _inherit = 'stock.picking'
+
 
     document_ids = fields.Many2many(
         'stock.document',
@@ -32,3 +33,21 @@ class stock_picking(models.Model):
         'document_id',
         'picking_id',
         'Documents')
+
+    name = fields.Char('name')
+    qty =fields.Integer('qty', compute='_calculate_qty')
+
+    @api.one
+    @api.depends('name')
+    def _calculate_qty(self):
+        picking_name = self.name
+        qty_mid = self.env.cr.execute(
+            """
+SELECT sum(product_uom_qty)
+FROM stock_move
+    INNER JOIN stock_picking ON stock_move.picking_id = stock_picking.id
+WHERE stock_picking.name = '"""+ picking_name + """'"""
+        )
+        qty_mid = self.env.cr.fetchall()
+        qty123 = int(qty_mid[0][0])
+        self.qty = qty123
