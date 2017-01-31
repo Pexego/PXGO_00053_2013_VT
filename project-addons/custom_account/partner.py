4a9b3b4d0d8ce8e186bad65cc58fcec03438da55
Omar <omarcs7r@gmail.com>
2017-01-31 19:33:00 +0100

    [IMP] custom_account: Mejora en el importe en curculación considerando también las remesas

--- project-addons/custom_account/partner.py
+++ project-addons/custom_account/partner.py
@@ -22,6 +22,7 @@
 
 from datetime import date
 from openerp import fields, models, api, exceptions, _
+from openerp.osv import fields as fields2
 
 
 class Partner(models.Model):
@@ -53,6 +54,45 @@ class Partner(models.Model):
             total += sline.price_subtotal
 
         self.pending_orders_amount = total
+
+    def _get_circulating_amount(self, cr, uid, ids, name, arg, context=None):
+        res = {}
+        for id in ids:
+            amount = 0.0
+            move_ids = self.pool.get('account.move.line').\
+                search(cr, uid, [('partner_id', 'child_of', [id]),
+                                 ('account_id.circulating', '=', True),
+                                 ('reconcile_id','=',False)])
+
+            for line in self.pool.get('account.move.line').browse(cr, uid, move_ids, context):
+                if line.currency_id:
+                    sign = line.amount_currency < 0 and -1 or 1
+                else:
+                    sign = (line.debit - line.credit) < 0 and -1 or 1
+                if line.reconcile_partial_id:
+                     amount += line.debit - line.credit
+                else:
+                    amount += sign * line.amount_residual
+
+            bank_line_ids = self.pool.get("bank.payment.line").\
+                search(cr, uid, [('partner_id', 'child_of', [id]),
+                                 ('order_id.payment_order_type', '=',
+                                  'debit')])
+            for bank_line in self.pool.get("bank.payment.line").\
+                    browse(cr, uid, bank_line_ids):
+                move_line = bank_line.transfer_move_line_id
+                if not move_line.reconcile_id and not move_line.partner_id:
+                    amount += bank_line.amount_currency
+
+            res[id] = amount
+
+        return res
+
+    _columns = {
+        'circulating_amount': fields2.\
+            function(_get_circulating_amount, method=True,
+                     string="Circulating amount", type="float"),
+    }
 
     @api.multi
     def _get_valid_followup_partners(self):
@@ -94,6 +134,8 @@ class Partner(models.Model):
     newsletter = fields.Boolean('Newsletter')
     pending_orders_amount = fields.Float(compute="_pending_orders_amount",
                                          string='Uninvoiced Orders')
+    circulating_amount = fields.Float(compute="_get_circulating_amount",
+                                      string="Circulating amount")
 
 
     @api.onchange("user_id")
