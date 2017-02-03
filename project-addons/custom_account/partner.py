f2e8c52c5dda0e0b6135066c4185c98e613dcd75
Omar <omarcs7r@gmail.com>
2017-02-03 15:13:17 +0100

    [REM] custom_account: Se elimina la personalización del importe en circulación

--- project-addons/custom_account/partner.py
+++ project-addons/custom_account/partner.py
@@ -55,45 +55,6 @@ class Partner(models.Model):
 
         self.pending_orders_amount = total
 
-    def _get_circulating_amount(self, cr, uid, ids, name, arg, context=None):
-        res = {}
-        for id in ids:
-            amount = 0.0
-            #~ move_ids = self.pool.get('account.move.line').\
-                #~ search(cr, uid, [('partner_id', 'child_of', [id]),
-                                 #~ ('account_id.circulating', '=', True),
-                                 #~ ('reconcile_id','=',False)])
-#~
-            #~ for line in self.pool.get('account.move.line').browse(cr, uid, move_ids, context):
-                #~ if line.currency_id:
-                    #~ sign = line.amount_currency < 0 and -1 or 1
-                #~ else:
-                    #~ sign = (line.debit - line.credit) < 0 and -1 or 1
-                #~ if line.reconcile_partial_id:
-                     #~ amount += line.debit - line.credit
-                #~ else:
-                    #~ amount += sign * line.amount_residual
-#~
-            #~ bank_line_ids = self.pool.get("bank.payment.line").\
-                #~ search(cr, uid, [('partner_id', 'child_of', [id]),
-                                 #~ ('order_id.payment_order_type', '=',
-                                  #~ 'debit')])
-            #~ for bank_line in self.pool.get("bank.payment.line").\
-                    #~ browse(cr, uid, bank_line_ids):
-                #~ move_line = bank_line.transfer_move_line_id
-                #~ if not move_line.reconcile_id and not move_line.partner_id:
-                    #~ amount += bank_line.amount_currency
-
-            res[id] = amount
-
-        return res
-
-    _columns = {
-        'circulating_amount': fields2.\
-            function(_get_circulating_amount, method=True,
-                     string="Circulating amount", type="float"),
-    }
-
     @api.multi
     def _get_valid_followup_partners(self):
         company_id = self.env.user.company_id.id
@@ -134,8 +95,6 @@ class Partner(models.Model):
     newsletter = fields.Boolean('Newsletter')
     pending_orders_amount = fields.Float(compute="_pending_orders_amount",
                                          string='Uninvoiced Orders')
-    circulating_amount = fields.Float(compute="_get_circulating_amount",
-                                      string="Circulating amount")
 
 
     @api.onchange("user_id")
