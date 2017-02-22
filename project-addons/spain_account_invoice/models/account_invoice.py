74760e0fed5b7e949c25e722105ebf55efd9223e
jgmanzanas <jgmanzanas@visiotech.es>
2017-02-22 08:44:44 +0100

    [DEV FIX] 'spain_account_invoice': Desarrollo incompleto de la ampliación del modulo para proveedores

--- project-addons/spain_account_invoice/models/account_invoice.py
+++ project-addons/spain_account_invoice/models/account_invoice.py
@@ -8,12 +8,21 @@ from openerp import models
 class AccountInvoice(models.Model):
     _inherit = 'account.invoice'
 
-    def _report_xls_fields(self, cr, uid, context=None):
-        return [
-            'number', 'date_invoice', 'partner_vat', 'partner_name', 'tax_base',
-            'tax_amount', 'tax_percent', 'tax_amount_rec', 'tax_amount_ret',
-            'amount_total', 'country_name', 'tax_description', 'fiscal_name'
-        ]
+    def _report_xls_fields(self, cr, uid, invoice_type, context=None):
+        datas = ""
+        if invoice_type == 'out_invoice':
+            datas = [
+                'number', 'date_invoice', 'partner_vat', 'partner_name', 'tax_base',
+                'tax_amount', 'tax_percent', 'tax_amount_rec', 'tax_amount_ret',
+                'amount_total', 'country_name', 'tax_description', 'fiscal_name'
+            ]
+        elif invoice_type == 'in_invoice':
+            datas = [
+                'date_invoice', 'number', 'supplier_number', 'partner_vat', 'partner_name', 'tax_base',
+                'tax_description', 'tax_amount', 'tax_amount_ret', 'amount_total', 'country_name'
+            ]
+
+        return datas
 
     def _report_xls_template(self, cr, uid, context=None):
         return {}
