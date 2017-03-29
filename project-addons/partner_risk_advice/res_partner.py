47623ef3ea287758ed3f434303854ccf92ccb583
jgmanzanas <jgmanzanas@visiotech.es>
2017-03-29 09:40:45 +0200

    ['IMP'] 'partner_risk_advice': Avisos de cliente en RMAs

--- project-addons/partner_risk_advice/res_partner.py
+++ project-addons/partner_risk_advice/res_partner.py
@@ -20,12 +20,60 @@
 ##############################################################################
 
 from openerp import fields, models, api
+from openerp.osv import osv, fields as fields2
+from openerp.tools.translate import _
+
+WARNING_MESSAGE = [
+                   ('no-message','No Message'),
+                   ('warning','Warning'),
+                   ('block','Blocking Message')
+                   ]
+
+WARNING_HELP = _('Selecting the "Warning" option will notify user with the message, Selecting "Blocking Message" will throw an exception with the message and block the flow. The Message has to be written in the next field.')
 
 
 class res_partner(models.Model):
     _inherit = "res.partner"
 
     risk_advice_ids = fields.One2many ("partner.risk.advice", "partner_id")
+    _columns = {
+        'rma_warn': fields2.selection(WARNING_MESSAGE, 'Invoice', help=WARNING_HELP, required=True),
+        'rma_warn_msg': fields2.text('Message for RMA'),
+    }
+
+    _defaults = {
+        'rma_warn': 'no-message',
+    }
+
+
+class crm_claim(osv.osv):
+    _inherit = 'crm.claim'
+
+    def onchange_partner_id(self, cr, uid, ids, part, email=False, context=None):
+        if not part:
+            return {}
+        warning = {}
+        title = False
+        message = False
+        partner = self.pool.get('res.partner').browse(cr, uid, part, context=context)
+        if partner.rma_warn != 'no-message':
+            title = _("Warning for %s") % partner.name
+            message = partner.rma_warn_msg
+            warning = {
+                'title': title,
+                'message': message,
+            }
+            if partner.rma_warn == 'block':
+                return {'value': {'partner_id': False}, 'warning': warning}
+        result = super(crm_claim, self).onchange_partner_id(cr, uid, ids, part, email, context=context)
+
+        if result.get('warning', False):
+            warning['title'] = title and title +' & '+ result['warning']['title'] or result['warning']['title']
+            warning['message'] = message and message + ' ' + result['warning']['message'] or result['warning']['message']
+
+        if warning:
+            result['warning'] = warning
+        return result
 
     # Descomentar para probar el cron con el botón en el formulario partner
     #@api.multi
