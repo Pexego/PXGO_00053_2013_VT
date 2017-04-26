8ee9d0b2be5860f33747e29694fdf697df075ed6
Omar <omarcs7r@gmail.com>
2017-04-26 16:49:41 +0200

    [FIX] product_pack: stock_depends por defecto a true

--- project-addons/product_pack/product.py
+++ project-addons/product_pack/product.py
@@ -217,6 +217,7 @@ class product_product(orm.Model):
 
     _defaults = {
         'pack_fixed_price': True,
+        'stock_depends': True
     }
 
     def write(self, cr, uid, ids, vals, context=None):
