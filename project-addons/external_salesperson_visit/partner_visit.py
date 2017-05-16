0feafab18af01432f4180899529be5ea2081a792
Naferreyra <nferreyra@visiotech.es>
2017-05-16 14:48:19 +0200

    [IMP] 'external_salesperson_visit': Optimización código, agrupaciones en vista visitas y añadido botón correspondiente en ficha de cliente

--- project-addons/external_salesperson_visit/partner_visit.py
+++ project-addons/external_salesperson_visit/partner_visit.py
@@ -19,8 +19,10 @@
 #
 ##############################################################################
 from openerp import models, fields, api, exceptions, _
-from openerp.exceptions import except_orm
+from openerp.osv import fields as fields2
+from openerp.exceptions import except_orm, ValidationError
 from datetime import datetime
+
 
 class partner_visit(models.Model):
     """ Model for Partner Visits """
@@ -28,6 +30,7 @@ class partner_visit(models.Model):
     _rec_name = "partner_id"
     _description = "Partner Visit"
     _order = "visit_date desc"
+    _inherit = ['mail.thread']
 
     create_date = fields.Datetime('Creation Date', readonly=True, default=fields.Datetime.now)
     visit_date = fields.Datetime('Visit Date', required=True)
@@ -41,52 +44,41 @@ class partner_visit(models.Model):
                                          compute='get_internal_salesperson', store=True)
     confirm_done = fields.Boolean('Done', default=False)
 
-    @api.multi
-    def validate_fields(self, self_data, changes):
+    @api.one
+    @api.constrains('confirm_done')
+    def validate_confirm_done(self):
         date_now = str(datetime.now())
-        res = {}
 
-        if 'visit_date' in changes:
-            visit_date = changes['visit_date']
-            if visit_date:
-                difference = datetime.strptime(date_now, '%Y-%m-%d %H:%M:%S.%f') - \
-                             datetime.strptime(visit_date, '%Y-%m-%d %H:%M:%S')
-                difference = difference.total_seconds() / float(60)
-                if self_data.visit_state == 'log' and difference < 0:
-                    raise except_orm(_('Invalid date'), _('Date must be lower than current date in a logged visit'))
-                elif self_data.visit_state == 'schedule' and difference > 0:
-                    raise except_orm(_('Invalid date'), _('Date must be bigger than current date in a scheduled visit'))
-            if 'confirm_done' in changes:
-                confirm_done = changes['confirm_done']
-                if confirm_done and difference < 0:
-                    raise except_orm(_('Invalid Action'), _('You cannot confirm the visit because schedule date '
-                                                            'is after current date'))
-        else:
-            if 'confirm_done' in changes:
-                confirm_done = changes['confirm_done']
-                # Case when visit_date has not been changed -> recalculate the difference with "self.visit_date"
-                difference = datetime.strptime(date_now, '%Y-%m-%d %H:%M:%S.%f') - \
-                    datetime.strptime(self_data.visit_date, '%Y-%m-%d %H:%M:%S')
-                difference = difference.total_seconds() / float(60)
-                if confirm_done and difference < 0:
-                    raise except_orm(_('Invalid Action'), _('You cannot confirm the visit because schedule date '
-                                                            'is after current date'))
-                elif confirm_done and difference > 0:
-                    res['visit_state'] = 'log'
-        return res
+        difference = datetime.strptime(date_now, '%Y-%m-%d %H:%M:%S.%f') - \
+                     datetime.strptime(self.visit_date, '%Y-%m-%d %H:%M:%S')
+        difference = difference.total_seconds() / float(60)
 
-    @api.model
-    def create(self, vals):
-        res = super(partner_visit, self).create(vals)
-        res_change = self.validate_fields(res, vals)
-        return res
+        if self.confirm_done:
+            if difference < 0:
+                raise ValidationError("You cannot confirm the visit because schedule date is after current date")
 
-    @api.multi
+        return True
+
+    @api.one
+    @api.constrains('visit_date')
+    def validate_visit_date(self):
+        date_now = str(datetime.now())
+
+        if self.visit_date:
+            difference = datetime.strptime(date_now, '%Y-%m-%d %H:%M:%S.%f') - \
+                         datetime.strptime(self.visit_date, '%Y-%m-%d %H:%M:%S')
+            difference = difference.total_seconds() / float(60)
+            if self.visit_state == 'log' and difference < 0:
+                raise ValidationError("Date must be lower than current date in a logged visit")
+            elif self.visit_state == 'schedule' and difference > 0:
+                raise ValidationError("Date must be bigger than current date in a scheduled visit")
+        return True
+
+    @api.one
     def write(self, datas):
+        if 'confirm_done' in datas and datas['confirm_done']:
+                datas['visit_state'] = 'log'
         res = super(partner_visit, self).write(datas)
-        res_change = self.validate_fields(self, datas)
-        if 'visit_state' in res_change:
-            self.visit_state = res_change['visit_state']
         return res
 
     @api.one
@@ -103,30 +95,47 @@ class partner_visit(models.Model):
 
     @api.one
     def send_email(self):
-        ir_model_data = self.env['ir.model.data']
-
+        mail_pool = self.env['mail.mail']
         context = self._context.copy()
         context['base_url'] = self.env['ir.config_parameter'].get_param('web.base.url')
 
         if self.visit_state == 'log':
-            template_id = ir_model_data.get_object_reference(
-                'external_salesperson_visit', 'email_template_logged_visits')[1]
+            template_id = self.env.ref(
+                'external_salesperson_visit.email_template_logged_visits', False)
         elif self.visit_state == 'schedule':
-            template_id = ir_model_data.get_object_reference(
-                'external_salesperson_visit', 'email_template_scheduled_visits')[1]
+            template_id = self.env.ref(
+                'external_salesperson_visit.email_template_scheduled_visits', False)
         else:
             template_id = 0
 
         if template_id:
-            mail_id = self.pool.get('email.template').send_mail(self._cr, self._uid,
-                                                                template_id, self.id, force_send=True, context=context)
+            mail_id = template_id.with_context(context).send_mail(self.id)
         else:
             mail_id = 0
 
         if mail_id:
+            mail_id_check = mail_pool.browse(mail_id)
+            mail_id_check.send()
             self.email_sent = True
         else:
             raise except_orm(_('Email Error'), _('Email has not been sent'))
 
         return True
+
+
+class res_partner(models.Model):
+    """ Inherits partner and adds Visit information in the partner form """
+    _inherit = 'res.partner'
+
+    def _visits_count(self, cr, uid, ids, field_name, arg, context=None):
+        visits = self.pool['partner.visit']
+        res = {}
+        for partner in self.browse(cr, uid, ids):
+            visit_ids = visits.search(cr, uid, [('partner_id', '=', partner.id), ('visit_state', '=', 'log')])
+            res[partner.id] = len(visit_ids)
+        return res
+
+    _columns = {
+        'visit_count': fields2.function(_visits_count, string="Visits", type="integer"),
+    }
 
