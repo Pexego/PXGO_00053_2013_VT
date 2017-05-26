7c97fe8b38ae4385ef4ff12b509835872548c694
jgmanzanas <jgmanzanas@visiotech.es>
2017-05-26 11:39:44 +0200

    [DEV] Modificado el metodo del pdf a get_pdfy

--- project-addons/flask_middleware_connector/events/invoice_events.py
+++ project-addons/flask_middleware_connector/events/invoice_events.py
@@ -18,6 +18,7 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
+from openerp import models
 from openerp.addons.connector.event import on_record_create, on_record_write, \
     on_record_unlink
 from openerp.addons.connector.queue.job import job
@@ -28,29 +29,17 @@ from ..unit.backend_adapter import GenericAdapter
 import xmlrpclib
 
 import ipdb
+import base64
 
 @middleware
 class InvoiceExporter(Exporter):
     _model_name = ['account.invoice']
 
-    def _render_invoice(self, invoice_id):
-        url = "http://172.18.2.3:9069"  # vuestra ip local y el puerto de Odoo
-        db = "visiotech"  # nombre de la bae de datos de Odoo
-        username = 'importacion'  # login de mi usuario
-        password = 'importacion'  # password de mi usuario
-
-        common = xmlrpclib.ServerProxy('{}/xmlrpc/2/common'.format(url))
-
-        uid = common.authenticate(db, username, password, {})
-        report = xmlrpclib.ServerProxy('{}/xmlrpc/2/report'.format(url))
-        result = report.render_report(
-            db, uid, password, 'account.report_invoice_custom',
-            (invoice_id, ))  # invoice_ids: Ids a imprimir en formato lista. Ej:[1], "account.report_invoice_custom" es el nombre del vuestro informe de facturacaso igual es otro
-        report_data = result['result']
-        return report_data
-
     def update(self, binding_id, mode):
         invoice = self.model.browse(binding_id)
+        report = self.env['report'].browse(invoice.id)
+        result = report.get_pdf('account.report_invoice_custom')
+        result_encode = base64.b64encode(result)
         vals = {'odoo_id': invoice.id,
                 'number': invoice.number,
                 'partner_id': invoice.partner_id.commercial_partner_id.id,
@@ -60,7 +49,7 @@ class InvoiceExporter(Exporter):
                 'state': invoice.state,
                 'subtotal_wt_rect': invoice.subtotal_wt_rect,
                 'total_wt_rect': invoice.total_wt_rect,
-                'pdf_file_data': self._render_invoice(invoice.id)}
+                'pdf_file_data': result_encode}
         if mode == 'insert':
             return self.backend_adapter.insert(vals)
         else:
