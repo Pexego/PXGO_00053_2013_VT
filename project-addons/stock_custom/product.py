1bd7525c8a757d7f3d5b4b99c6b3b446e70b6f32
Naferreyra <nferreyra@visiotech.es>
2017-08-18 14:55:39 +0200

    [FIX] 'stock_custom': Quitado filtro de "puede ser vendido" activo por defecto en apartado Stock Nuevo

--- project-addons/stock_custom/product.py
+++ project-addons/stock_custom/product.py
@@ -62,7 +62,6 @@ class ProductProduct(models.Model):
             'view_mode': 'tree,form',
             'view_type': 'form',
             'context': {'tree_view_ref': 'product.product_product_tree_view',
-                        'search_default_filter_to_sell': 1,
                         'readonly_by_pass': ['lst_price', 'list_price2', 'list_price3']},
             'res_model': 'product.product',
             'type': 'ir.actions.act_window',
