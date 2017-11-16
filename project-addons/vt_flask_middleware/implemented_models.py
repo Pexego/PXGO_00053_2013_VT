a0fa39004fbf77ac9a0246c213c9bd61d2fc1ffc
jgmanzanas <jgmanzanas@visiotechsecurity.com>
2017-11-16 17:04:54 +0100

    [IMP] 'vt_flask_middleware_connector': Incluido albaranes y productos de albaranes como modelos en flask

--- project-addons/vt_flask_middleware/implemented_models.py
+++ project-addons/vt_flask_middleware/implemented_models.py
@@ -5,17 +5,20 @@ from customer import Customer
 from invoice import Invoice
 from commercial import Commercial
 from rma import RmaStatus, RmaStage, Rma, RmaProduct
+from picking import Picking, PickingProduct
+#from post import Post
 MODELS_CLASS = {
     'invoice': Invoice, 'customer': Customer, 'product': Product,
     'productcategory': ProductCategory, 'rmastatus': RmaStatus,
-    'rmastage': RmaStage, 'rma': Rma, 'rmaproduct': RmaProduct,
-    'country': Country, 'commercial': Commercial, 'productbrand': ProductBrand,
-    'productbrandcountryrel': ProductBrandCountryRel}
+    'rmastage': RmaStage, 'rma': Rma, 'picking': Picking, 'pickingproduct': PickingProduct,
+    'rmaproduct': RmaProduct, 'country': Country, 'commercial': Commercial,
+    'productbrand': ProductBrand, 'productbrandcountryrel': ProductBrandCountryRel}#, 'post': Post}
 
 MASTER_CLASSES = {'commercial': Commercial,'productcategory': ProductCategory,
                   'rmastatus': RmaStatus, 'rmastage': RmaStage,
                   'country': Country, 'productbrand': ProductBrand}
 
 DEPENDENT_CLASSES = {'invoice': Invoice, 'customer': Customer, 'product': Product,
+                     'picking': Picking, 'pickingproduct': PickingProduct,
                      'rma': Rma, 'rmaproduct': RmaProduct,
                      'productbrandcountryrel': ProductBrandCountryRel}
