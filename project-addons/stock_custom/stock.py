69734cdd013822da9c151e1c1f088d962aafbcb5
jgmanzanas <jgmanzanas@visiotechsecurity.com>
2017-11-22 17:44:35 +0100

    [FIX] 'stock_custom': Solucionado problema con los imports

--- project-addons/stock_custom/stock.py
+++ project-addons/stock_custom/stock.py
@@ -18,7 +18,7 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
-from openerp import models, fields, exceptions, api, _ipdb
+from openerp import models, fields, exceptions, api, _
 
 
 class StockHistory(models.Model):
