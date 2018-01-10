397f8b01ef6e2f5f739bf4662f3e17466a14d0fb
jgmanzanas <jgmanzanas@visiotechsecurity.com>
2018-01-10 17:08:39 +0100

    [IMP] 'flask_middleware_connector': Se ha incluido en la logica el write 2 estados mas. Estos estados no estan en Odoo y son unicamente estados informativos para los clientes en la web.

--- project-addons/flask_middleware_connector/events/invoice_events.py
+++ project-addons/flask_middleware_connector/events/invoice_events.py
@@ -30,11 +30,12 @@ import xmlrpclib
 
 import base64
 
+
 @middleware
 class InvoiceExporter(Exporter):
     _model_name = ['account.invoice']
 
-    def update(self, binding_id, mode):
+    def update(self, binding_id, mode, state=None):
         invoice = self.model.browse(binding_id)
         report = self.env['report'].browse(invoice.id)
         result = report.get_pdf('account.report_invoice_custom')
@@ -45,11 +46,20 @@ class InvoiceExporter(Exporter):
                 'client_ref': invoice.name or "",
                 'date_invoice': invoice.date_invoice,
                 'date_due': invoice.date_due,
-                'state': invoice.state,
                 'subtotal_wt_rect': invoice.subtotal_wt_rect,
                 'total_wt_rect': invoice.total_wt_rect,
-                'pdf_file_data': result_encode}
+                'pdf_file_data': result_encode,
+                'payment_mode_id': invoice.payment_mode_id.name}
+        if state:
+            vals['state'] = state
+        else:
+            vals['state'] = invoice.state
+            res = _new_state_invoice(invoice, vals)
+            if res:
+                vals['state'] = res
         if mode == 'insert':
+            if invoice.returned_payment and invoice.state == 'open':
+                vals['state'] = 'returned'
             return self.backend_adapter.insert(vals)
         else:
             return self.backend_adapter.update(binding_id, vals)
@@ -64,21 +74,52 @@ class InvoiceAdapter(GenericAdapter):
     _middleware_model = 'invoice'
 
 
+def _new_state_invoice(invoice, vals):
+    res = None
+    if 'payment_mode_id' in vals and vals.get('payment_mode_id', False) == 'Recibo domicialiado' \
+            or invoice.payment_mode_id.name == 'Recibo domiciliado':
+        if 'returned_payment' in vals and vals.get('returned_payment', False) or invoice.returned_payment:
+            if 'state' in vals and vals.get('state', False) == 'open' and \
+                    invoice.returned_payment or ('returned_payment' in vals and vals.get('returned_payment', False)):
+                res = 'returned'
+        elif 'state' in vals and vals.get('state', False) == 'paid':
+            for payment in invoice.payment_ids:
+                for payment_account in payment.move_id.line_id:
+                    if payment_account.account_id.code == '43120000' \
+                                        and payment_account.account_id.user_type.code == 'receivable' \
+                                        and payment_account.reconcile_id:
+                        for reconcile_line in payment_account.reconcile_id.line_id:
+                            if reconcile_line.move_id != payment.move_id and reconcile_line.credit != 0:
+                                res = 'paid'
+                        break
+                    else:
+                        res = 'remitted'
+                if res == 'paid':
+                    break
+    return res
+
+
 @on_record_write(model_names='account.invoice')
-def delay_write_invoice(session, model_name, record_id, vals):
+def delay_write_invoice(session, model_name, record_id, vals, checked_state=False):
     invoice = session.env[model_name].browse(record_id)
     up_fields = ["number", "client_ref", "date_invoice", "state", "partner_id",
-                 "date_due", "subtotal_wt_rect", "subtotal_wt_rect"]
+                 "date_due", "subtotal_wt_rect", "subtotal_wt_rect", "payment_ids",
+                 "returned_payment", "payment_mode_id"]
 
     if invoice.partner_id and invoice.commercial_partner_id.web:
         job = session.env['queue.job'].search([('func_string', 'not like', '%confirm_one_invoice%'),
                                                ('func_string', 'like', '%, ' + str(invoice.id) + ')%'),
                                                ('model_name', '=', model_name)], order='date_created desc', limit=1)
         if job:
+            if not checked_state:
+                vals['state'] = _new_state_invoice(invoice, vals)
+                if not vals['state']:
+                    del vals['state']
             if vals.get('state', False) == 'open' and 'unlink_invoice' in job[0].func_string:
                 export_invoice.delay(session, model_name, record_id, priority=5)
-            elif vals.get('state', False) == 'paid':
-                update_invoice.delay(session, model_name, record_id, priority=10, eta=60)
+            elif vals.get('state', False) in ('paid', 'returned', 'remitted'):
+                update_invoice.delay(session, model_name, record_id, state=vals.get('state', False),
+                                     priority=10, eta=60)
             elif vals.get('state', False) == 'cancel' and 'unlink_invoice' not in job[0].func_string:
                 unlink_invoice.delay(session, model_name, record_id, priority=15)
             elif invoice.state == 'open':
@@ -99,9 +140,9 @@ def export_invoice(session, model_name, record_id):
 
 @job(retry_pattern={1: 10 * 60, 2: 20 * 60, 3: 30 * 60, 4: 40 * 60,
                     5: 50 * 60})
-def update_invoice(session, model_name, record_id):
+def update_invoice(session, model_name, record_id, state=None):
     invoice_exporter = _get_exporter(session, model_name, record_id, InvoiceExporter)
-    return invoice_exporter.update(record_id, "update")
+    return invoice_exporter.update(record_id, "update", state)
 
 
 @job(retry_pattern={1: 10 * 60, 2: 20 * 60, 3: 30 * 60, 4: 40 * 60,
