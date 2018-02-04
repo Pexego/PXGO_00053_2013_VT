4cd2a452b929d98173dea14a5a6d358b406ba5b4
Jesús <jgmanzanas@visiotechsecurity.com>
2018-02-04 19:40:56 +0100

    Nueva precio en producto y ordenación de RMAs (#80)
    * [IMP] 'flask_middleware_connector': Se envia pvm_price a flask
    
    * [IMP] 'vt_flask_middleware': Se crea un campo "pvm" en Flask
    
    * [IMP] 'purchase_proposal': Se ha incluido un campo "pvm_price" que tiene una tarifa concreta para ciertos clinetes. Este precio se calcula con el precio de coste y un porcentaje que se saca de la tarifa PVM.
    
    * [FIX] 'crm_claim_rma_custom': Se han modificados los valores del selection del campo priority para evitar problemas de ordenacion
    
    * [FIX] 'custom_account': Inherit de la funcion de correos de ordenes de pago para que se envien a "email2" del "partner" siempre que este exista, sino se enviaran a "email"
    
    * [FIX] 'purchase_proposal': Se ha modificado el objeto del campo pvm_price de product.product a product.template. Tambien se ha modificado la funcion _get_pvm()
    
    * [FIX] 'purchase_proposal': Eliminado codigo comentado
    
    * [FIX] 'purchase_proposal': Eliminado ipdb y un caracter fallido

--- project-addons/custom_account/payment.py
+++ project-addons/custom_account/payment.py
@@ -5,10 +5,56 @@
 from openerp import models, fields, api, exceptions, _
 
 
-class PaymentOrder(models.Model):
+class PaymentOrderLine(models.Model):
 
     _inherit = 'payment.line'
 
     _order = 'partner_name'
 
     partner_name = fields.Char(related='partner_id.name', store=True)
+
+
+class PaymentOrder(models.Model):
+
+    _inherit = 'payment.order'
+
+    @api.multi
+    def action_sent(self):
+        mail_pool = self.env['mail.mail']
+        mail_ids = self.env['mail.mail']
+        ctx = {}
+        for order in self:
+            if order.not_send_emails:
+                continue
+            partners = {}
+            for line in order.line_ids:
+                if line.partner_id.email2 or line.partner_id.email:
+                    if partners.get(line.partner_id):
+                        partners[line.partner_id].append(line)
+                    else:
+                        partners[line.partner_id] = [line]
+
+            for partner_data in partners:
+                template = self.env.ref('account_banking_sepadd_groupby_partner.payment_order_advise_partner', False)
+                ctx = dict(self._context)
+                ctx.update({
+                    'partner_email': partner_data.email,
+                    'partner_email2': partner_data.email2,
+                    'partner_id': partner_data.id,
+                    'partner_name': partner_data.name,
+                    'lines': partners[partner_data]
+                })
+                mail_id = template.with_context(ctx).send_mail(order.id)
+                mail_ids += mail_pool.browse(mail_id)
+
+            order.not_send_emails = True
+        self = self.with_context(ctx)
+        res = super(PaymentOrder, self).action_sent()
+
+        for order in self:
+            order.not_send_emails = False
+
+        if mail_ids:
+            mail_ids.send()
+
+        return res
