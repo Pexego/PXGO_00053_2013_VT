9d329e6ca6efc2fd66bf8c4dca87d8ca36590113
rcerrillo2 <rcerrillo@visiotechsecurity.com>
2018-05-10 11:16:01 +0200

    [FIX]'account_payment_term_and_mode': Arreglada función de desmarcar seguimiento en devoluciones

--- project-addons/account_payment_term_and_mode/models/payment_return.py
+++ project-addons/account_payment_term_and_mode/models/payment_return.py
@@ -6,62 +6,16 @@ class PaymentReturn(models.Model):
 
     _inherit = 'payment.return'
 
-    # Override button function
     @api.multi
     def action_confirm(self):
-        self.ensure_one()
-        # Check for incomplete lines
-        if self.line_ids.filtered(lambda x: not x.move_line_ids):
-            raise UserError(
-                _("You must input all moves references in the payment "
-                  "return."))
-        invoices_returned = self.env['account.invoice']
-        move = {
-            'name': '/',
-            'ref': _('Return %s') % self.name,
-            'journal_id': self.journal_id.id,
-            'date': self.date,
-            'company_id': self.company_id.id,
-            'period_id': (self.period_id.id or self.period_id.with_context(
-                company_id=self.company_id.id).find(self.date).id),
-        }
-        move_id = self.env['account.move'].create(move)
-        for return_line in self.line_ids:
-            lines2reconcile = return_line.move_line_ids.mapped(
-                'reconcile_id.line_id')
-            invoices_returned |= self._get_invoices(lines2reconcile)
-            for move_line in return_line.move_line_ids:
-                move_amount = self._get_move_amount(return_line, move_line)
-                move_line2 = move_line.copy(
-                    default={
-                        'move_id': move_id.id,
-                        'debit': move_amount,
-                        'name': move['ref'],
-                        'credit': 0,
-                    })
-                lines2reconcile |= move_line2
-                move_line2.copy(
-                    default={
-                        'debit': 0,
-                        'credit': move_amount,
-                        'account_id':
-                            self.journal_id.default_credit_account_id.id,
-                    })
-                # Break old reconcile
-                move_line.reconcile_id.unlink()
-            # Make a new one with at least three moves
-            lines2reconcile.reconcile_partial()
-            return_line.write(
-                {'reconcile_id': move_line2.reconcile_partial_id.id})
+        res = super(PaymentReturn, self).action_confirm()
 
-            # Mark negative line as no-followup
-            for line_id in lines2reconcile:
-                if line_id.debit - line_id.credit < 0:
-                    line_id.blocked = True
+        reconcile_partial = [line.reconcile_partial_id.id for line in self.move_id.line_id if line.reconcile_partial_id]
 
-        # Mark invoice as payment refused
-        invoices_returned.write(self._prepare_invoice_returned_vals())
-        move_id.button_validate()
-        self.write({'state': 'done', 'move_id': move_id.id})
+        # Mark negative line as no-followup
+        for deb_line in self.line_ids.partner_id.unreconciled_aml_ids:
+            if (deb_line.debit - deb_line.credit < 0) \
+                    and (deb_line.reconcile_partial_id.id == reconcile_partial[0]):
+                deb_line.write({'blocked': True})
 
-        return True
+        return res
