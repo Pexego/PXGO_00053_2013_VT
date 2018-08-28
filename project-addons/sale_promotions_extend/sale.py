d1f0d21cda9bc294e4e40d0a4ea51da66fbf1785
rcerrillo2 <rcerrillo@visiotechsecurity.com>
2018-08-28 14:37:52 +0200

    [FIX]'sale_promotions_extend': cambiada la forma de coger las tasas para las líneas de promoción para evitar generarlas vacías.

--- project-addons/sale_promotions_extend/sale.py
+++ project-addons/sale_promotions_extend/sale.py
@@ -87,7 +87,7 @@ class SaleOrder(osv.osv):
         if order.state == 'reserve':
             order.order_reserve()
 
-        taxes = order.order_line[0].tax_id
+        taxes = order.order_line.filtered(lambda l: l.tax_id.id is not False)[0].tax_id
         for line in order.order_line:
             if line.promotion_line:
                 line.tax_id = taxes
