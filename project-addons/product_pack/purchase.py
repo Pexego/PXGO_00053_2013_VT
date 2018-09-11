c6cce99d9348477f5f62fd74469fcaccbd6d4d01
Naferreyra <nferreyra@visiotech.es>
2018-09-11 17:36:00 +0200

    [FIX] 'product_pack': Reordenación de las líneas de un pedido de compra para que no se coloque en la última posición la primera línea insertada

--- project-addons/product_pack/purchase.py
+++ project-addons/product_pack/purchase.py
@@ -41,7 +41,7 @@ class purchase_order_line(orm.Model):
             'purchase.order.line', 'pack_parent_line_id', 'Lines in pack'
         ),
     }
-    _order = 'order_id desc, sequence, id'
+    _order = 'order_id desc, id, sequence'
     _defaults = {
         'pack_depth': 0,
     }
