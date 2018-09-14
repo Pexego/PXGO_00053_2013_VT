840cd9fdd3d9ef87ffeaa8fc9d666c91655a451c
rcerrillo2 <rcerrillo@visiotechsecurity.com>
2018-09-14 09:44:32 +0200

    [IMP]'phone_numbers': quitar contactos de la comprobación

--- project-addons/phone_numbers/res_partner.py
+++ project-addons/phone_numbers/res_partner.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 
 from openerp import models, fields, api, _
-from openerp.tools.safe_eval import safe_eval
 from openerp.exceptions import Warning
 import logging
 
@@ -12,42 +11,41 @@ import phonenumbers
 class ResPartner(models.Model):
     _inherit = "res.partner"
 
-    _phone_fields = ['phone','mobile']
+    phone_fields = ['phone', 'mobile']
 
     @api.multi
     def write(self, vals):
 
         vals_reformated = vals
-        if 'phone' in vals or 'mobile' in vals:
-            vals_reformated = self._format_numbers(vals)
+        if not self.parent_id.id:
+            if 'phone' in vals or 'mobile' in vals:
+                vals_reformated = self._format_numbers(vals)
 
         return super(ResPartner, self).write(vals_reformated)
 
     @api.multi
     def create(self, vals):
-
+        # TODO: mirar lo del is_company, poner and o separar en dos if o algo asi
         vals_reformated = vals
-        if 'phone' in vals or 'mobile' in vals:
-            vals_reformated = self._format_numbers(vals)
+        if not self.parent_id.id and 'is_company' in vals:
+            if 'phone' in vals or 'mobile' in vals:
+                vals_reformated = self._format_numbers(vals)
 
         return super(ResPartner, self).create(vals_reformated)
 
     def _format_numbers(self,vals):
 
-        import ipdb
-        ipdb.set_trace()
-
         countrycode = self.country_id.code
 
-        for field in self._phone_fields:
-            if field in vals:
+        for field in self.phone_fields:
+            if field in vals and vals[field]:
                 try:
                     res_parse = phonenumbers.parse(vals.get(field), countrycode)
                     vals[field] = phonenumbers.format_number(res_parse, phonenumbers.PhoneNumberFormat.E164)
                 except Exception, e:
                     raise Warning(
-                    ("Cannot reformat the phone number '%s' to "
-                    "international format. Error message: %s")
+                    ("Cannot format the phone number '%s' to "
+                    "international format. Error: %s")
                     % (vals.get(field), e))
 
         return vals
