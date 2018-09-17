64be0838c87f6f2663d565df00b2837813ba9de3
rcerrillo2 <rcerrillo@visiotechsecurity.com>
2018-09-17 09:03:37 +0200

    [IMP]'sale_custom': comprobación de gastos de envío.

--- project-addons/sale_custom/models/sale.py
+++ project-addons/sale_custom/models/sale.py
@@ -113,4 +113,12 @@ class SaleOrder(models.Model):
                 message = _('Please, validate shipping address.')
                 raise exceptions.Warning(message)
 
+            shipping_cost_line = False
+            for line in self.order_line:
+                if line.product_id.categ_id.name == 'Portes':
+                    shipping_cost_line = True
+            if not shipping_cost_line:
+                message = _('Please, introduce a shipping cost line.')
+                raise exceptions.Warning(message)
+
         return super(SaleOrder, self).action_button_confirm()
