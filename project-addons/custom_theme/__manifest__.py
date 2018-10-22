b269b3c6daadc3f056531ba2c20c85e18a842a23
rcerrillo2 <rcerrillo@visiotechsecurity.com>
2018-10-22 12:48:12 +0200

    [IMP]'custom_theme': nuevo tema

---
+++ project-addons/custom_theme/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    "name": "custom theme",
+    "description": "A custom theme",
+    "version": "0.1",
+    "category": "Theme/Backend",
+    "data": [
+        "views/assets.xml",
+    ],
+    "depends": [
+        "web"
+    ],
+    "auto_install": False,
+    "installable": True,
+}
