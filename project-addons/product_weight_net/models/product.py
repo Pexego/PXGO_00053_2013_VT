36f6249bb770b390a4a0c209bd1a06693d4d2464
rcerrillo2 <rcerrillo@visiotechsecurity.com>
2018-11-22 16:35:05 +0100

    [UPD]'product_weight_net': añadido campo de peso neto al producto y a la ficha

--- project-addons/product_weight_net/models/product.py
+++ project-addons/product_weight_net/models/product.py
@@ -2,11 +2,7 @@ from odoo import api, fields, models
 import odoo.addons.decimal_precision as dp
 
 
-class ProductProduct(models.Model):
-    _inherit = 'product.product'
+class ProductTemplate(models.Model):
 
-    weight = fields.Float('Gross Weight', digits_compute=dp.get_precision('Stock Weight'),
-                          help="The gross weight in Kg.")
     weight_net = fields.Float('Net Weight', digits_compute=dp.get_precision('Stock Weight'),
-                              help="The net weight in Kg.")
-
+                              help="The net weight in Kg")
