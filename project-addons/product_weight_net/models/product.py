62136334c4bd32044eaef4cf30edeea2ed9da5a4
rcerrillo2 <rcerrillo@visiotechsecurity.com>
2018-11-27 13:26:57 +0100

    [FIX]'product_weight_net': añadido inherit

--- project-addons/product_weight_net/models/product.py
+++ project-addons/product_weight_net/models/product.py
@@ -4,5 +4,7 @@ import odoo.addons.decimal_precision as dp
 
 class ProductTemplate(models.Model):
 
+    _inherit = 'product.template'
+
     weight_net = fields.Float('Net Weight', digits_compute=dp.get_precision('Stock Weight'),
                               help="The net weight in Kg")
