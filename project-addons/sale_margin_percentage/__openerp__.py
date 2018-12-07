af1a0fdacc7ce7d9702404590ab9ae9843a2d1c4
rcerrillo2 <rcerrillo@visiotechsecurity.com>
2018-12-07 14:03:17 +0100

    [MIG]'sale_margin_percentage': Reorganizados directorios para v11

--- project-addons/sale_margin_percentage/__openerp__.py
+++ project-addons/sale_margin_percentage/__openerp__.py
@@ -21,13 +21,13 @@
 
 {
     'name': 'Percentage of margins in Sales Orders',
-    'version':'1.0',
-    'category' : 'Sales Management',
+    'version': '1.0',
+    'category': 'Sales Management',
     'description': """
     """,
-    'author':'Pexego',
-    'depends':['sale', 'stock_deposit', 'pmp_landed_costs', 'product_pack'],
-    'data':["sale_view.xml", "sale_report_view.xml"],
+    'author': 'Pexego',
+    'depends': ['sale', 'stock_deposit', 'pmp_landed_costs', 'product_pack'],
+    'data': ["views/sale_view.xml", "views/sale_report_view.xml"],
     'auto_install': False,
     'installable': True,
 }
