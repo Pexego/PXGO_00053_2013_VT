00d8634e2fccf7b02e3f1d2c2d123f3101ace36b
rcerrillo2 <rcerrillo@visiotechsecurity.com>
2019-02-22 11:11:36 +0100

    [MIG]'crm_claim_summary_report': migración a v11

--- project-addons/crm_claim_summary_report/models/claim_line.py
+++ project-addons/crm_claim_summary_report/models/claim_line.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 ##############################################################################
 #
 #    Copyright 2016 Comunitea Servicios Tecnológicos S.L.
@@ -19,24 +18,24 @@
 #
 ##############################################################################
 
-from openerp import models, fields
+from odoo import models, fields
 
 
 class ClaimLine(models.Model):
 
-    _inherit = "claim.line"
+    _inherit = 'claim.line'
 
     printable = fields.Boolean("Printable", default=True)
 
     claim_origine = fields.Selection([('broken_down', 'Broken down product'),
-         ('not_appropiate', 'Not appropiate product'),
-         ('cancellation', 'Order cancellation'),
-         ('damaged', 'Damaged delivered product'),
-         ('error', 'Shipping error'),
-         ('exchange', 'Exchange request'),
-         ('lost', 'Lost during transport'),
-         ('other', 'Other')
-         ],
-        'Claim Subject',
-        required=True,
-        help="To describe the line product problem")
+                                      ('not_appropiate', 'Not appropiate product'),
+                                      ('cancellation', 'Order cancellation'),
+                                      ('damaged', 'Damaged delivered product'),
+                                      ('error', 'Shipping error'),
+                                      ('exchange', 'Exchange request'),
+                                      ('lost', 'Lost during transport'),
+                                      ('other', 'Other')
+                                      ],
+                                     'Claim Subject',
+                                     required=True,
+                                     help="To describe the line product problem")
