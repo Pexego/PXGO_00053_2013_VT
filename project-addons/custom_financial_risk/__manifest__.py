b23cd3cab83287759e9c3c18c3cda39da4026c7b
JesusVMayor <jesus@comunitea.com>
2019-03-01 14:47:32 +0100

    [ADD]custom_financial_risk: Añadido módulo para gestionar riesgo circulante.

---
+++ project-addons/custom_financial_risk/__manifest__.py
@@ -0,0 +1,18 @@
+# © 2019 Comunitea
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    'name': 'Financial risk customizations',
+    'version': '11.0.1.0.0',
+    'category': 'Accounting',
+    'author': 'Comunitea',
+    'website': 'www.comunitea.com',
+    'license': 'AGPL-3',
+    'depends': [
+        'account_financial_risk',
+    ],
+    'data': [
+        'views/account.xml',
+        'views/res_partner.xml'
+    ],
+    'installable': True,
+}
