79d6bfeda4112bf39680f7df61b49ad3b6ddf843
Omar (Comunitea) <omar@comunitea.com>
2019-03-04 15:08:16 +0100

    [REM] partner_risk__stock_reserve__rel,partner_risk_insurance. [FIX][IMP] block_invoices,crm_claim_phonecall,custom_account,partner_sale_graphic,sale_custom: Correcciones de dependencias y cambio de funcionalidad personalizada de módulos

--- project-addons/custom_account/__manifest__.py
+++ project-addons/custom_account/__manifest__.py
@@ -17,7 +17,7 @@
                 'account_payment_mode', 'sale', 'purchase',
                 'account_financial_risk', 'account_payment_order',
                 'sale_margin_percentage', 'account_banking_sepa_direct_debit',
-                'stock_reserve_sale', 'sales_team',
+                'stock_reserve_sale', 'sales_team', 'partner_risk_insurance',
                 'custom_partner', 'account_payment_return',
                 'sale_advance_payment', 'account_banking_sepa_mail',
                 'account_credit_control', 'stock_picking_invoice_link',
@@ -29,7 +29,6 @@
              'security/ir.model.access.csv',
              'views/stock_view.xml',
              'views/account_payment.xml',
-             #'crm_view.xml', TODO: Migrar a crm_claim_phonecall
              'views/purchase_view.xml',
              'report/sale_report_view.xml',
              'data/custom_account_data.xml',
