1b59927fe9d5fac1c2592bf5afbb468d8e7ec570
David <dmora@visiotechsecurity.com>
2019-03-20 12:51:38 +0100

    [DEV]'custom_report_link':report_claim

--- project-addons/custom_report_link/__manifest__.py
+++ project-addons/custom_report_link/__manifest__.py
@@ -27,7 +27,7 @@
     'website': 'www.comunitea.com',
     "depends": ['sale', 'purchase', 'account', 'stock',
                 'stock_reserve_sale', 'stock_custom', 'sale_proforma_report',
-                'stock_picking_report_valued'],
+                'stock_picking_report_valued', 'crm_claim'],
     "data": [
         "data/report_paperformat.xml",
         "views/custom_layout.xml",
@@ -36,6 +36,7 @@
         "views/report_purchase_order.xml",
         "views/report_stock_picking.xml",
         "views/report_stock_picking_valued.xml",
-        "views/report_invoice.xml"],
+        "views/report_invoice.xml",
+        "views/report_claim.xml"],
     "installable": True
 }
