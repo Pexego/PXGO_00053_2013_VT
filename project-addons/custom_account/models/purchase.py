98fd4c23bcc7cab86d9034960a65ed67e64b34f3
rcerrillo2 <rcerrillo@visiotechsecurity.com>
2019-03-22 14:49:40 +0100

    [UPD]'custom_account': recolocación de campos

--- project-addons/custom_account/models/purchase.py
+++ project-addons/custom_account/models/purchase.py
@@ -1,6 +1,6 @@
 # Copyright 2019 Omar Castiñeira, Comunitea Servicios Tecnológicos S.L.
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-from odoo import fields, models, _, exceptions, api
+from odoo import fields, models, _, exceptions
 
 
 class PurchaseOrder(models.Model):
@@ -21,20 +21,6 @@ class PurchaseOrder(models.Model):
                     write({'state': 'cancel'})
         super().button_cancel()
 
-    @api.depends('order_line.date_planned')
-    def _minimum_planned_date(self):
-        for po in self:
-            po.minimum_planned_date = min(po.order_line.mapped('date_planned'))
-
-    @api.multi
-    def _set_minimum_planned_date(self):
-        for po in self:
-            for line in po.order_line:
-                line.date_planned = po.minimum_planned_date
-
-    minimum_planned_date = fields.Date("Expected Date", compute="_minimum_planned_date",
-                                       inverse="_set_minimum_planned_date", store=True,
-                                       help="This is computed as the minimum scheduled date of all purchase order lines' products.")
     manufacture_date = fields.Date("Manufacture Date",
                                    help="Date when it was manufactured")
     prepared_merchandise_date = \
