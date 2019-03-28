bc4527ad8a4dfeea05ccbe6fccbf480ffffe5ac3
JesusVMayor <jesus@comunitea.com>
2019-03-28 09:37:54 +0100

    [FIX]vt_flask_middleware: Recuperados parametros de configuración originales.

--- project-addons/vt_flask_middleware/config.py
+++ project-addons/vt_flask_middleware/config.py
@@ -1,20 +1,21 @@
 import os
 
+
 class Config(object):
-    DEBUG = True
+    DEBUG = False
     TESTING = False
     SECRET_KEY = 'A0Zr18h/3yX R~XHH!jmN]LWX/,?RT'
     DATABASE = {
                 'engine': 'playhouse.pool.PooledPostgresqlExtDatabase',
                 'name': 'middleware',
                 'user': 'comunitea',
-                'port': '5432',
+                'port': '5434',
                 'host': 'localhost',
                 'max_connections': None,
                 'autocommit': True,
                 'autorollback': True,
                 'stale_timeout': 600}
 
-    NOTIFY_URL = ""
+    NOTIFY_URL = "https://www.visiotechsecurity.com/?option=com_sync&task=sync.syncOdoo"
     NOTIFY_USER = os.environ.get('NOTIFY_USER')
     NOTIFY_PASSWORD = os.environ.get('NOTIFY_PASSWORD')
