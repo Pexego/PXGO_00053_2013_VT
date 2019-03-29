7ec80b041e86aa3dbc323a8057044dde27e2ddcd
David <dmora@visiotechsecurity.com>
2019-03-29 14:16:00 +0100

    [IMP]'product_discontinued':descatalogar producto

--- project-addons/product_discontinued/__manifest__.py
+++ project-addons/product_discontinued/__manifest__.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 {
     "name": "Discontinued Product",
     "version": "1.0",
--- project-addons/product_discontinued/models/product.py
+++ project-addons/product_discontinued/models/product.py
@@ -1,33 +1,37 @@
-# -*- coding: utf-8 -*-
-
-from openerp import fields, models, api, _
-from openerp.exceptions import ValidationError
+from odoo import fields, models, api, _
+from odoo.exceptions import ValidationError
 
 
 class ProductTemplate(models.Model):
-    _inherit = 'product.product'
-
+    _inherit = 'product.template'
     discontinued = fields.Boolean(string="Discontinued", default=False, help="If marked, product not more available")
 
     @api.multi
     @api.constrains('discontinued')
     def allow_discontinued(self):
         for item in self:
-            if item.product_tmpl_id and item.discontinued:
-                if (item.state == 'end' and item.qty_available == 0):
+            if (item.qty_available != 0):
+                if item.discontinued:   # and if (item.state != 'end' or item.qty_available != 0):
+                    raise ValidationError(_(
+                        "The product can not be discontinued. Currently exist stock or the status is not - End of lifecycle"))
+            elif item.qty_available == 0 and (item.product_variant_count is 0):
+                if item.discontinued:
                     item.sale_ok = False
                     item.purchase_ok = False
-                else:
-                    raise ValidationError(_("The product can not be discontinued. Currently exist stock or the status is not - End of lifecycle"))
-            return True
 
     @api.multi
     @api.onchange('discontinued')
     def warning_catalog(self):
         for item in self:
-            if item.product_tmpl_id:
+            if item.qty_available != 0:
                 if not item.discontinued:
                     item.sale_ok = True
-                    if item.qty_available == 0:
+            elif item.qty_available == 0:
+                if item.discontinued:
+                    item.sale_ok = False
+                    item.purchase_ok = False
+                if item.product_variant_count is 1:
+                    if not item.discontinued:
+                        item.sale_ok = True
                         result = {'warning': {'title': _('Warning'), 'message': _('The product does not have stock.')}}
                         return result
