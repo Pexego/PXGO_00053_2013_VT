16c9c567994f32ad21217284570b033fb541483b
David <dmora@visiotechsecurity.com>
2019-04-15 16:00:53 +0200

    [FIX]'custom_report_link':Borrrar_ipdb

--- project-addons/custom_report_link/models/account_invoice.py
+++ project-addons/custom_report_link/models/account_invoice.py
@@ -8,8 +8,6 @@ class AccountInvoice(models.Model):
     _inherit = 'account.invoice'
 
     def invoice_print(self):
-        import ipdb
-        ipdb.set_trace()
         super().invoice_print()
         return self.env.ref(
             'account.report_invoice_custom').report_action(self)
