e4aa9a39c38b194efddf112d1add39651f673499
Omar (Comunitea) <omar@comunitea.com>
2019-05-06 18:32:37 +0200

    [MIG] pmp_landed_costs: Migrado de la 8.0 a la 11.0

--- project-addons/pmp_landed_costs/models/__init__.py
+++ project-addons/pmp_landed_costs/models/__init__.py
@@ -1,4 +1,5 @@
 # Copyright 2019 Omar Castiñeira, Comunitea Servicios Tecnológicos S.L.
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from . import models
+from . import product
+from . import stock_landed_costs
