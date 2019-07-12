ace9b0971b876df1c7684a2cb1245fafb98ff981
rcerrillo2 <rcerrillo@visiotechsecurity.com>
2019-07-12 13:15:22 +0200

    [FIX]'product_states': añadido grupo de mrp para que sat vea el fabricante

--- project-addons/product_states/__manifest__.py
+++ project-addons/product_states/__manifest__.py
@@ -25,7 +25,7 @@
     'description': """Adds states to the product""",
     'author': 'Pexego Sistemas Informáticos',
     'website': 'www.pexego.es',
-    "depends": ["base", "product", "sale_stock", "stock", "purchase", "product_manufacturer"],
+    "depends": ["base", "product", "sale_stock", "stock", "purchase", "product_manufacturer", "mrp"],
     "data": ["views/product_view.xml"],
     "installable": True
 }
