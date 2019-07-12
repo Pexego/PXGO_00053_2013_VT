34e08f691ffcae532c848c3be0e61cc4fdf96d8f
Omar (Comunitea) <omar@comunitea.com>
2019-07-12 10:47:04 +0200

    [IMP] custom_account: Se mejorar la búsqueda de apuntes en las devoluciones de cobro

--- project-addons/custom_account/models/payment_return.py
+++ project-addons/custom_account/models/payment_return.py
@@ -1,6 +1,6 @@
 # Copyright 2019 Omar Castiñeira, Comunitea Servicios Tecnológicos S.L.
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-from odoo import api, models
+from odoo import api, models, _
 
 
 class PaymentReturn(models.Model):
@@ -15,3 +15,26 @@ class PaymentReturn(models.Model):
                 move_line.blocked = True
 
         return res
+
+
+class PaymentReturnLine(models.Model):
+    _inherit = "payment.return.line"
+
+    @api.multi
+    def match_move_lines(self):
+        for line in self:
+            domain = line.partner_id and [
+                ('partner_id', '=', line.partner_id.id)] or []
+            domain.extend([
+                ('account_id.internal_type', '=', 'receivable'),
+                ('reconciled', '=', True),
+                '|',
+                ('name', 'like', line.reference),
+                ('ref', 'like', line.reference),
+            ])
+            move_lines = self.env['account.move.line'].search(domain)
+            if move_lines:
+                line.move_line_ids = move_lines.ids
+                if not line.concept:
+                    line.concept = (_('Move lines: %s') %
+                                    ', '.join(move_lines.mapped('name')))
