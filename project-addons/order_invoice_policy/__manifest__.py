f294a8782f09e3e2d310b0a41b0a4782dcc8a2ae
Omar (Comunitea) <omar@comunitea.com>
2019-07-30 18:57:26 +0200

    [FIX] order_invoice_policy: Se considera también el caso de forzar facturado en el nuevo estado de facturación de la venta

--- project-addons/order_invoice_policy/__manifest__.py
+++ project-addons/order_invoice_policy/__manifest__.py
@@ -6,7 +6,7 @@
     'category': 'sale',
     'description': """""",
     'author': 'Visiotech',
-    "depends": ['base', 'sale', 'sale_stock'],
+    "depends": ['base', 'sale_force_invoiced', 'sale_stock'],
     "data": ['security/sale_security.xml',
              'views/sale_order_view.xml'],
     "installable": True
