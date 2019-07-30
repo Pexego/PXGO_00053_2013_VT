19e54ecd143dd66fc6be81c169c4af20c80474ef
rcerrillo2 <rcerrillo@visiotechsecurity.com>
2019-07-30 13:55:23 +0200

    [FIX]prospective_customer: arreglada función de confirmación

--- project-addons/prospective_customer/models/sale_order.py
+++ project-addons/prospective_customer/models/sale_order.py
@@ -1,11 +1,12 @@
-from odoo import models, fields
+from odoo import models, fields, api
 
 
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
 
+    @api.multi
     def action_confirm(self):
         for order in self:
             if order.partner_id.prospective:
                 order.partner_id.write({'active': True, 'prospective': False})
-        super().action_button_confirm()
+        super().action_confirm()
