8c976fa44f0bdce90cf863b977e9a6dc040a22e5
rcerrillo2 <rcerrillo@visiotechsecurity.com>
2019-09-23 09:28:52 +0200

    [IMP]flask_middleware_connector: mejorada la búsqueda de trabajos duplicados

--- project-addons/flask_middleware_connector/models/stock_picking/common.py
+++ project-addons/flask_middleware_connector/models/stock_picking/common.py
@@ -145,18 +145,16 @@ class StockMoveListener(Component):
                 record.with_delay(priority=5, eta=240).unlink_pickingproduct()
 
     def on_stock_move_change(self, record):
-        if record.product_id.show_stock_outside:
-            if self.env['queue.job'].search([('state', '=', 'pending')]):
-                pending_ids = self.env['queue.job'].search([('state', '=', 'pending')]).mapped(lambda r: int(r.record_ids[0]))
-                if pending_ids and record.product_id.id in pending_ids:
-                    record.product_id.with_delay(priority=12, eta=30).update_product()
-            else:
-                record.product_id.with_delay(priority=12, eta=30).update_product()
+        record._cr.execute("select trim(trailing ']' from trim(leading '[' from record_ids))::int from queue_job "
+                           "where state = 'pending' and job_function_id = 486 and model_name = 'product.product'")
+        res = record._cr.fetchall()
+        products = [x for x in map(lambda l: l[0], res)]
 
+        if record.product_id.show_stock_outside and record.product_id.id not in products:
+            record.product_id.with_delay(priority=12, eta=30).update_product()
         packs = self.env['mrp.bom.line'].search([('product_id', '=', record.product_id.id)]).mapped('bom_id')
         for pack in packs:
-            pending_ids = self.env['queue.job'].search([('state', '=', 'pending')]).mapped(lambda r: int(r.record_ids[0]))
-            if pending_ids and pack.product_tmpl_id.product_variant_ids.id not in pending_ids:
+            if pack.product_tmpl_id.product_variant_ids.id not in products:
                 pack.product_tmpl_id.product_variant_ids.with_delay(priority=12, eta=30).update_product()
 
 
