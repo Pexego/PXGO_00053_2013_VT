8eb1f5d239e3a7f503c2b86d16f562c92c855084
Albertocanal <acanal@visiotechsecurity.com>
2019-09-25 12:28:08 +0200

    [FIX] stock_custom: Arreglado fallo en el listado de Productos nuevos.

--- project-addons/stock_custom/models/product.py
+++ project-addons/stock_custom/models/product.py
@@ -1,7 +1,7 @@
 # © 2016 Comunitea
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-import seaborn as sns
-import pandas as pd
+#import seaborn as sns
+#import pandas as pd
 from matplotlib import pyplot as plt
 from odoo import api, fields, models, _, exceptions
 from io import BytesIO
@@ -16,7 +16,6 @@ matplotlib.use('Agg')
 class ProductTemplate(models.Model):
 
     _inherit = 'product.template'
-
     date_start = fields.Date(
         "Start date",
         default=lambda *a: (datetime.now() - relativedelta(months=6)).strftime(
@@ -244,9 +243,6 @@ class ProductProduct(models.Model):
             'name': _('Stock New'),
             'view_mode': 'tree,form',
             'view_type': 'form',
-            'context': {'tree_view_ref': 'product.product_product_tree_view',
-                        'readonly_by_pass': ['lst_price', 'list_price2',
-                                             'list_price3', 'list_price4']},
             'res_model': 'product.product',
             'type': 'ir.actions.act_window',
         }
