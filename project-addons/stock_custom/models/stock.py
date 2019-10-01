898e781ab6c1037f41300c07f816b81a82dd9f58
Albertocanal <acanal@visiotechsecurity.com>
2019-10-01 10:29:51 +0200

    [IMP] stock_custom: Añadido el campo cliente y albarán en la vista de los números de serie

--- project-addons/stock_custom/models/stock.py
+++ project-addons/stock_custom/models/stock.py
@@ -264,7 +264,8 @@ class StockProductionLot(models.Model):
         compute='_compute_partner_id',
         help='The last customer in possession of the product')
     lot_notes = fields.Text('Notes')
-
+    order_id= fields.Many2one('sale.order',string='Order',compute='_compute_partner_id')
+    stock_operation_name = fields.Many2one('stock.picking',string='Picking',compute='_compute_partner_id')
     def _compute_partner_id(self):
         pass
         for lot in self:
@@ -273,6 +274,10 @@ class StockProductionLot(models.Model):
             if move_line:
                 lot.partner_id = \
                     move_line.picking_id.partner_id.commercial_partner_id
+                lot.order_id=self.env['sale.order'].search(
+                    [('name','=',move_line.picking_id.origin),('partner_id','=',lot.partner_id.id)], limit=1)
+                lot.stock_operation_name= self.env['stock.picking'].search(
+                    [('name', '=', move_line.picking_id.name)], limit=1)
             else:
                 lot.partner_id = False
 
