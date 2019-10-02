f148facc22ffbfb863ba18ab28f61eae5c5bed88
Albertocanal <acanal@visiotechsecurity.com>
2019-10-02 09:33:19 +0200

    [FIX] transportation: Cambios en la forma de hacer función

--- project-addons/transportation/models/sale.py
+++ project-addons/transportation/models/sale.py
@@ -41,19 +41,14 @@ class SaleOrder(models.Model):
     @api.multi
     @api.onchange('partner_id')
     def onchange_partner_id_transporter(self):
-        transporter_ids=list()
-        all_transporters=self.env['transportation.transporter'].search([])
-        partnerCountry=self.partner_id.country_id
-        for x in all_transporters:
-            if partnerCountry in x.country_group_id.country_ids:
-                transporter_ids.append(x.id)
-
+        transporter_ids=self.env['transportation.transporter'].search(
+            [('country_group_id.country_ids', 'in', [self.partner_id.country_id.id])])
         if transporter_ids:
-            if self.transporter_id.id not in transporter_ids:
+            if self.transporter_id not in transporter_ids:
                 self.transporter_id = False
-            return {'domain': {'transporter_id': [('id', 'in', transporter_ids)]}}
-        all_transporters_ids = [x.id for x in self.env['transportation.transporter'].search([])]
-        return {'domain': {'service_id': [('id', 'in', all_transporters_ids)]}}
+            return {'domain': {'transporter_id': [('id', 'in', transporter_ids.mapped("id"))]}}
+        all_transporters_ids = self.env['transportation.transporter'].search([])
+        return {'domain': {'transporter_id': [('id', 'in', all_transporters_ids.mapped("id"))]}}
 
 
 
