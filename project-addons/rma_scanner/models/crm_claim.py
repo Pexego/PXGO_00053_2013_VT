fa6a7acc0afce847bae88ab3b858c10ab989d9de
rcerrillo2 <rcerrillo@visiotechsecurity.com>
2019-10-31 17:41:49 +0100

    [IMP]rma_scanner: nuevo módulo para escanear y recibir RMAs

---
+++ project-addons/rma_scanner/models/crm_claim.py
@@ -0,0 +1,29 @@
+from odoo import api, models, fields, _
+
+
+class CrmClaim(models.Model):
+    _inherit = 'crm.claim'
+
+    @api.multi
+    def find_claim_by_number_using_barcode(self, barcode):
+        barcode = barcode.replace("-", "/")
+        barcode = barcode.replace("'", "-")
+        claim = self.search([('number', '=', barcode)], limit=1)
+
+        if claim:
+            if claim.stage_id.name != "Pendiente de recibir":
+                message = "The {} is already received".format(claim.number)
+                self.env.user.notify_warning(title="RMA scanned", message=message, sticky=True)
+            else:
+                claim.stage_id = self.env["crm.claim.stage"].search([('name', '=', 'Recibido')])
+                claim.date_received = fields.Date.today()
+                message = "{} received".format(claim.number)
+                self.env.user.notify_info(title="RMA scanned", message=message)
+        else:
+            message = "The RMA {} doesn't exists".format(barcode)
+            self.env.user.notify_warning(message=message, sticky=True)
+
+        action = self.env.ref('rma_scanner.crm_claim_rma_scanner')
+        result = action.read()[0]
+
+        return result
