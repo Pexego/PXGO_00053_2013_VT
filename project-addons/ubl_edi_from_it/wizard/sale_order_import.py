ad3671414da550cf1f944836f41decc5b3a15ee0
Omar (Comunitea) <omar@comunitea.com>
2019-11-25 20:54:02 +0100

    [FIX] ubl_edi_from_it: No actualizar al importar ventas en LX

--- project-addons/ubl_edi_from_it/wizard/sale_order_import.py
+++ project-addons/ubl_edi_from_it/wizard/sale_order_import.py
@@ -1,6 +1,8 @@
 # Copyright 2019 Omar Castiñeira, Comunitea Servicios Tecnológicos S.L.
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-from odoo import models, api
+from odoo import models, api, _
+from base64 import b64decode
+from odoo.exceptions import UserError
 
 
 class SaleOrderImport(models.TransientModel):
@@ -20,3 +22,15 @@ class SaleOrderImport(models.TransientModel):
         so_vals = super()._prepare_order(parsed_order, price_source)
         so_vals['not_sync'] = True
         return so_vals
+
+    def import_order_button(self):
+        self.ensure_one()
+        order_file_decoded = b64decode(self.order_file)
+        parsed_order = self.parse_order(
+            order_file_decoded, self.order_filename, self.partner_id)
+        if not parsed_order.get('lines'):
+            raise UserError(_(
+                "This order doesn't have any line !"))
+
+        return self.create_order_return_action(
+            parsed_order, self.order_filename)
