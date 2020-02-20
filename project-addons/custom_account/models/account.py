b5e0848660fb2c8052baac6b5f879ddc62bf89b5
Albertocanal <acanal@visiotechsecurity.com>
2020-02-20 14:53:21 +0100

    [FIX] custom_account: arrastrado el comercial asociado al cliente en las facturas

--- project-addons/custom_account/models/account.py
+++ project-addons/custom_account/models/account.py
@@ -165,6 +165,7 @@ class AccountInvoice(models.Model):
         if p:
             self.attach_picking = p.commercial_partner_id.attach_picking
             self.team_id = p.commercial_partner_id.team_id.id
+            self.user_id = p.commercial_partner_id.user_id.id
             if self.type != "in_invoice":
                 self.currency_id = p.commercial_partner_id.\
                     property_purchase_currency_id.id or self.env.user.company_id.currency_id.id
