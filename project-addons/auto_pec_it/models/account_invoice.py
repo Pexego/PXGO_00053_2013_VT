d690fad3751bb3b9f19a449f63d2fdd34b8f4848
rcerrillo2 <rcerrillo@visiotechsecurity.com>
2020-03-18 15:59:35 +0100

    [IMP]auto_pec_it: añadido caso de cancelación de factura

--- project-addons/auto_pec_it/models/account_invoice.py
+++ project-addons/auto_pec_it/models/account_invoice.py
@@ -1,4 +1,4 @@
-from odoo import models, fields, api, _
+from odoo import models, fields, api, _, exceptions
 from odoo.addons.queue_job.job import job
 from datetime import datetime, timedelta
 
@@ -27,6 +27,33 @@ class AccountInvoice(models.Model):
                 invoice.sudo().invoice_pec_jobs_ids |= job
             else:
                 invoice.auto_fatturapa_error = 'Attachment already done'
+        return res
+
+    @api.multi
+    def unlink(self):
+        for invoice in self:
+            if invoice.fatturapa_state:
+                raise exceptions.Warning(
+                    _("You cannot delete an invoice already registered at the PEC")
+                )
+        return super(AccountInvoice, self).unlink()
+
+    @api.multi
+    def _cancel_invoice_jobs(self):
+        for queue in self.sudo().mapped('invoice_pec_jobs_ids'):
+            if queue.state == 'started':
+                return False
+            elif queue.state in ('pending', 'enqueued', 'failed'):
+                queue.unlink()
+        return True
+
+    @api.multi
+    def action_cancel(self):
+        if not self._cancel_invoice_jobs():
+            raise exceptions.Warning(_(
+                'You can not cancel this invoice because'
+                ' there is a job running!'))
+        res = super(AccountInvoice, self).action_cancel()
         return res
 
     @job(default_channel='root.invoice_validate_pec')
