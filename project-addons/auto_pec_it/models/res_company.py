6a7e4d2402ce43fc079feddf5e6cd7d42558bf48
rcerrillo2 <rcerrillo@visiotechsecurity.com>
2020-03-18 16:51:22 +0100

    [IMP]auto_pec_it: añadidas traducciones

--- project-addons/auto_pec_it/models/res_company.py
+++ project-addons/auto_pec_it/models/res_company.py
@@ -5,7 +5,7 @@ class ResCompany(models.Model):
 
     _inherit = 'res.company'
 
-    auto_pec_template = fields.Many2one(
-        comodel_name='ir.actions.actions',
-        help='This report will be automatically included in the created XML')
+    auto_pec_template = fields.Many2one(string="PEC template",
+                                        comodel_name='ir.actions.actions',
+                                        help='This report will be automatically included in the created XML')
     pec_delay_time = fields.Float(string="PEC Delay time")
