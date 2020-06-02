a1a85b1846e3d272e441b65b7ab47546fa7173d0
Albertocanal <acanal@visiotechsecurity.com>
2020-06-02 12:41:41 +0200

    [IMP] vt_flask_middleware: añadido campos precio de coste de un producto y campo que indica si el cliente tiene de plazo de pago prepago

--- project-addons/vt_flask_middleware/product.py
+++ project-addons/vt_flask_middleware/product.py
@@ -77,6 +77,7 @@ class Product(SyncModel):
     sale_in_groups_of = FloatField(default=1.0)
     replacement_id = IntegerField()
     date_next_incoming = DateTimeField(formats=['%Y-%m-%d %H:%M:%S'])
+    cost_price = FloatField(default=0.0)
 
     def __unicode__(self):
         return self.name
