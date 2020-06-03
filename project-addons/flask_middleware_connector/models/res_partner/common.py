46546b44ae6366187ebd9712b34e44f6d602104d
Albertocanal <acanal@visiotechsecurity.com>
2020-06-03 09:36:48 +0200

    [IMP] flask_middleware_connector: movido método para calcular si un cliente es prepago desde módulo prepaid_order_discount

--- project-addons/flask_middleware_connector/models/res_partner/common.py
+++ project-addons/flask_middleware_connector/models/res_partner/common.py
@@ -248,6 +248,17 @@ class ResPartner(models.Model):
             return exporter.delete_category_rel(self.id)
         return True
 
+    @api.multi
+    def prepaid_payment_term(self):
+        # Obtener ids de plazo de pago que requieran prepagar (Prepago y Pago inmediato)
+        prepaid_ids = []
+        prepaid_ids.extend([self.env['account.payment.term'].
+                           with_context(lang='en_US').search([('name', 'ilike', 'Prepaid')]).id])
+        prepaid_ids.extend([self.env.ref('account.account_payment_term_immediate').id])
+        for partner in self:
+            # Si el plazo de pago del cliente coincide con alguno de esos ids, devolver True
+            return partner.property_payment_term_id.id in prepaid_ids
+
 
 class PartnerCategoryListener(Component):
     _name = 'partner.category.event.listener'
