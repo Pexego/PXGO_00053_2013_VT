1b7a7b1b191c82a077e0591116202fca64af8ff9
Albertocanal <acanal@visiotechsecurity.com>
2020-06-15 12:54:26 +0200

    [FIX] flask_middleware_connector: Refactorización completa del wizard de flask, añadida exportación de modelos inexistentes hasta el momento y arreglado que se generen más trabajos de los necesarios en la exportación de las traducciones

--- project-addons/flask_middleware_connector/models/translation/common.py
+++ project-addons/flask_middleware_connector/models/translation/common.py
@@ -16,22 +16,24 @@ class IrTranslationListener(Component):
         if len(name) == 2:
             if name[0] in models.keys() and name[1] in models.get(name[0], []):
                 record.with_delay(priority=1).export_translation()
-                record.web = True
+                record.with_context({'no_update': True}).web = True
 
     def on_record_write(self, record, fields=None):
         models = eval(self.env['ir.config_parameter'].sudo().get_param('translations.to.export'))
         name = record.name.split(',')
-        if len(name) == 2:
+        no_update = self.env.context.get('no_update',False)
+        if not no_update and len(name) == 2:
             if name[0] in models.keys() and name[1] in models.get(name[0], []):
                 if record.web:
                     record.with_delay(priority=2, eta=10).update_translation(fields=fields)
                 else:
                     record.with_delay(priority=1).export_translation()
-                    record.web = True
+                    record.with_context({'no_update':True}).web = True
 
     def on_record_unlink(self, record):
         if len(record.name.split(',')) == 2 and record.web:
             record.with_delay().unlink_translation()
+            record.with_context({'no_update':True}).web = False
 
 
 class IrTranslation(models.Model):
