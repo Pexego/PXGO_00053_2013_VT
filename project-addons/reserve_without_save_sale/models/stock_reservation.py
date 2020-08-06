d478697450059e67123d4d867625e6fbef2d0df3
rcerrillo2 <rcerrillo@visiotechsecurity.com>
2020-08-06 11:48:41 +0200

    [IMP]crm_claim_rma, purchase_picking, reserve_without_save_sale: eliminada mensajería de stock.reservation

--- project-addons/reserve_without_save_sale/models/stock_reservation.py
+++ project-addons/reserve_without_save_sale/models/stock_reservation.py
@@ -125,16 +125,7 @@ class StockReservation(models.Model):
                          'sale_line_id': current_sale_line_id,
                          'date_validity': date_validity
                          })
-                reservation.message_post(
-                    body=_("Reserva modificada. Estado '%s'") %
-                    reservation.state)
         return moves
-
-    def release(self):
-        res = super().release()
-        for reserve in self:
-            reserve.message_post(body=_("Reserva liberada."))
-        return res
 
     @api.model
     def delete_orphan_reserves(self):
