3c4430cc3a0d20a2bce9ed24849f0ebba83705a3
rcerrillo2 <rcerrillo@visiotechsecurity.com>
2020-08-18 15:03:32 +0200

    [FIX]separate_purchase_orders: comentado cálculo de stock fabricando por lentitud

--- project-addons/separate_purchase_orders/models/product.py
+++ project-addons/separate_purchase_orders/models/product.py
@@ -1,21 +1,21 @@
 from odoo import models, fields, api
 
-
-class ProductTemplate(models.Model):
-    _inherit = 'product.template'
-
-    @api.multi
-    def _get_in_production_stock(self):
-        res= super(ProductTemplate, self)._get_in_production_stock()
-        for product in self:
-            if product.product_variant_ids:
-                order_lines = self.env["purchase.order.line"].search([('product_id', 'in', product.product_variant_ids.ids),
-                                                       ('order_id.state', '=', 'purchase_order'),('order_id.completed_purchase','=',False)])
-                qty = 0.0
-                for order_line in order_lines:
-                    qty += order_line.production_qty
-                product.qty_in_production += qty
-        return res
+# TODO: comentado hasta encontrar una forma mejor de calcularlo
+# class ProductTemplate(models.Model):
+#     _inherit = 'product.template'
+#
+#     @api.multi
+#     def _get_in_production_stock(self):
+#         res= super(ProductTemplate, self)._get_in_production_stock()
+#         for product in self:
+#             if product.product_variant_ids:
+#                 order_lines = self.env["purchase.order.line"].search([('product_id', 'in', product.product_variant_ids.ids),
+#                                                        ('order_id.state', '=', 'purchase_order'),('order_id.completed_purchase','=',False)])
+#                 qty = 0.0
+#                 for order_line in order_lines:
+#                     qty += order_line.production_qty
+#                 product.qty_in_production += qty
+#         return res
 
 class ProductProduct(models.Model):
     _inherit = 'product.product'
