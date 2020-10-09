a19598d427302f443f45236ac93cb66c5c6c200e
rcerrillo2 <rcerrillo@visiotechsecurity.com>
2020-10-09 12:20:07 +0200

    [IMP]block_invoices: añadida dependencia

--- project-addons/block_invoices/__manifest__.py
+++ project-addons/block_invoices/__manifest__.py
@@ -8,7 +8,7 @@
     'description': """Block sales and invoices for customers from due dates.""",
     'author': 'Alberto Luengo, Comunitea',
     'website': 'luengocabanillas.com, http://www.comunitea.com',
-    "depends": ['sale', 'stock_account', 'custom_account'],
+    "depends": ['sale', 'stock_account', 'custom_account', 'invoice_update_data'],
     "data": ['views/res_company_view.xml', 'views/res_partner_view.xml',
              'views/sale_view.xml', 'views/account_invoice_view.xml',
              'data/ir_cron.xml', 'data/parameters.xml', 'security/block_invoices.xml'],
