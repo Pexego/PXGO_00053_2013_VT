1ba041362148f8145694657ecea144ddc8544e34
rcerrillo2 <rcerrillo@visiotechsecurity.com>
2020-11-23 11:37:48 +0100

    [FIX]custom_advance_payment: añadido recorrer varios anticipos

--- project-addons/custom_advanced_payment/models/account_payment.py
+++ project-addons/custom_advanced_payment/models/account_payment.py
@@ -7,13 +7,13 @@ class AccountPayment(models.Model):
     @api.multi
     def post(self):
         res = super(AccountPayment, self).post()
-        if self.sale_id.invoice_ids:
-            debit_receipt_param = self.env['ir.config_parameter'].sudo().get_param('debit.receipt.account.ids')
-            account_id = int(debit_receipt_param.split(',')[1])
-            move_line = self.move_line_ids.filtered(lambda m: m.account_id.id == account_id)
-            # Paid the invoices with the advanced payment
-            for invoice in self.sale_id.invoice_ids:
-                if invoice.state == 'open' and move_line.amount_residual:
-                    invoice.assign_outstanding_credit(move_line.id)
-
+        for payment in self:
+            if payment.sale_id.invoice_ids:
+                debit_receipt_param = self.env['ir.config_parameter'].sudo().get_param('debit.receipt.account.ids')
+                account_id = int(debit_receipt_param.split(',')[1])
+                move_line = payment.move_line_ids.filtered(lambda m: m.account_id.id == account_id)
+                # Paid the invoices with the advanced payment
+                for invoice in payment.sale_id.invoice_ids:
+                    if invoice.state == 'open' and move_line.amount_residual:
+                        invoice.assign_outstanding_credit(move_line.id)
         return res
