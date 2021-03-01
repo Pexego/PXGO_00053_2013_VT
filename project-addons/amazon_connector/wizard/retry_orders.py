d0d77e41055fa823673af0c090783223dc4b651e
Albertocanal <acanal@visiotechsecurity.com>
2021-03-01 12:42:59 +0100

    [FIX] amazon_connector: corregido error en el wizard

--- project-addons/amazon_connector/wizard/retry_orders.py
+++ project-addons/amazon_connector/wizard/retry_orders.py
@@ -18,8 +18,8 @@ class RetryAmazonOrders(models.TransientModel):
     @api.multi
     def retry_orders(self):
         amazon_orders=self.amazon_orders.mapped('amazon_order')
-        amazon_orders.retry_order()
-        amazon_orders.process_order()
+        amazon_orders.filtered(lambda o: o.state=='error').retry_order()
+        amazon_orders.filtered(lambda o: o.state=='read').process_order()
 
 
 
