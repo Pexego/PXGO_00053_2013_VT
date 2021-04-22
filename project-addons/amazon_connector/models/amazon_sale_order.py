36352d70c1f65afb34082e0652308b1d8036ded0
Albertocanal <acanal@visiotechsecurity.com>
2021-04-22 17:21:25 +0200

    [IMP][FIX] amazon_connector: fusionar facturas si hay más de una y solucionado errores en depósitos

--- project-addons/amazon_connector/models/amazon_sale_order.py
+++ project-addons/amazon_connector/models/amazon_sale_order.py
@@ -43,9 +43,9 @@ class AmazonSaleOrder(models.Model):
             if order.deposits:
                 order.deposits_count = len(order.deposits)
                 order.sale_deposits = order.deposits.mapped('sale_id')
-                order.invoice_deposits = order.deposits.mapped('invoice_id')
+                order.invoice_deposits = self.env['account.invoice'].search([('amazon_order','=',order.id)])
                 order.sale_deposits_count = len(order.sale_deposits)
-                order.invoice_deposits_count = len(order.invoice_deposits)
+                order.invoice_deposits_count = len(set(order.invoice_deposits))
 
     def action_view_sales(self):
         action = self.env.ref('sale.action_quotations').read()[0]
@@ -385,15 +385,18 @@ class AmazonSaleOrder(models.Model):
                         else:
                             invoices_ids = deposits.create_invoice()
                             order.state = "invoice_created"
-                            for invoice in self.env['account.invoice'].browse(invoices_ids):
-                                invoice.write({'name': order.name,'amazon_order':order.id})
-                                for line in invoice.invoice_line_ids:
-                                    o_line = order.order_line.filtered(lambda l: l.product_id == line.product_id)[0]
-                                    line.write({'invoice_line_tax_ids': [(6, 0, o_line.tax_id.ids)], 'price_unit': o_line.price_unit, 'discount': 0})
-                                invoice._onchange_invoice_line_ids()
-                                if not order.warning_price:
-                                    invoice.action_invoice_open()
-                                    order.state = 'invoice_open'
+                            invoices = self.env['account.invoice'].browse(invoices_ids)
+                            if len(invoices)>1:
+                                allinvoices = invoices.do_merge(keep_references=False)
+                                invoices = self.env['account.invoice'].browse(list(allinvoices))
+                            invoices.write({'name': order.name,'amazon_order':order.id})
+                            for line in invoices.invoice_line_ids:
+                                o_line = order.order_line.filtered(lambda l: l.product_id == line.product_id)[0]
+                                line.write({'invoice_line_tax_ids': [(6, 0, o_line.tax_id.ids)], 'price_unit': o_line.price_unit, 'discount': 0})
+                            invoices._onchange_invoice_line_ids()
+                            if not order.warning_price:
+                                invoices.action_invoice_open()
+                                order.state = 'invoice_open'
 
                     else:
                         self.env.user.notify_warning(message=_("There are no deposit for this order"))
