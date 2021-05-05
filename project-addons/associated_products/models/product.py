0c042b1448b7a1e1e23dde7977f17f825e51a534
Albertocanal <acanal@visiotechsecurity.com>
2021-05-05 12:28:18 +0200

    [IMP] associated_products: búsqueda por productos equivalentes

--- project-addons/associated_products/models/product.py
+++ project-addons/associated_products/models/product.py
@@ -27,6 +27,16 @@ class Product(models.Model):
 
     associated_product_ids = fields.One2many('product.associated', 'product_id', "Associated products")
     equivalent_product_ids = fields.One2many('product.equivalent',  'product_id', "Equivalent products")
+    equivalent_products = fields.One2many('product.product',compute="_compute_equivalent_products",search="_search_equivalent_products")
+
+    def _compute_equivalent_products(self):
+        for product in self:
+            product.equivalent_products = self.env['product.equivalent'].search(['|',('product_id', '=', product.id),('equivalent_id', '=', product.id)])
+
+    def _search_equivalent_products(self, operator, value):
+        prod_equiv = self.env['product.equivalent'].search(['|',('product_id.default_code', operator, value),('equivalent_id.default_code', operator, value)])
+        products = prod_equiv.mapped('product_id') + prod_equiv.mapped('equivalent_id')
+        return [('id', 'in', products.ids)]
 
 
 class AssociatedProducts(models.Model):
@@ -51,3 +61,14 @@ class EquivalentProduct(models.Model):
 
     product_id = fields.Many2one('product.product', "Product", required=True)
     equivalent_id = fields.Many2one('product.product', "Equivalent product", required=True)
+
+    @api.model
+    def name_search(self, name='', args=None, operator='ilike', limit=100):
+        import ipdb
+        ipdb.set_trace()
+        args = args or []
+        recs = self.browse()
+        if name:
+            recs = self.search(['|',('product_id.default_code', operator, name),('equivalent_id.default_code', operator, name)] + args, limit=limit)
+
+        return recs.name_get()
