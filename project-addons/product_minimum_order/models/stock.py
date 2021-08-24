408b09ba67a20aafca2c4417c67e4445d7e30d29
rcerrillo2 <rcerrillo@visiotechsecurity.com>
2021-08-24 13:53:11 +0200

    [FIX]product_minimum_order: eliminado cambio para cambiarlo de rama

--- project-addons/product_minimum_order/models/stock.py
+++ project-addons/product_minimum_order/models/stock.py
@@ -10,8 +10,7 @@ class StockPicking(models.Model):
         super().action_copy_reserv_qty()
         for pick in self:
             for move in pick.move_lines:
-                if move.reserved_availability % move.product_id.sale_in_groups_of != 0\
-                        and (move.sale_line_id and not move.sale_line_id.product_id.is_pack):
+                if move.reserved_availability % move.product_id.sale_in_groups_of != 0:
                     min_qty = move.product_id.sale_in_groups_of
                     # Assign the maximum qty available base on the minimum qty required
                     move.qty_confirmed = min_qty * (int(move.reserved_availability / min_qty))
@@ -20,8 +19,7 @@ class StockPicking(models.Model):
     def action_accept_confirmed_qty(self):
         for pick in self:
             for move in pick.move_lines:
-                if move.qty_confirmed % move.product_id.sale_in_groups_of != 0\
-                        and (move.sale_line_id and not move.sale_line_id.product_id.is_pack):
+                if move.qty_confirmed % move.product_id.sale_in_groups_of != 0:
                     min_qty = move.product_id.sale_in_groups_of
                     # Assign the maximum qty available base on the minimum qty required
                     move.qty_confirmed = min_qty * (int(move.qty_confirmed / min_qty))
