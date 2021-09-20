1913f89ce60aed267a594272dedac470230ecc01
rcerrillo2 <rcerrillo@visiotechsecurity.com>
2021-09-20 14:50:38 +0200

    [IMP]sim_manager: mejorada la búsqueda de las tarjetas

--- project-addons/sim_manager/models/stock.py
+++ project-addons/sim_manager/models/stock.py
@@ -9,12 +9,12 @@ class StockMove(models.Model):
         res = super().write(vals)
         if 'lots_text' in vals:
             if self.picking_id.picking_type_id.code == 'outgoing':
-                for pkg_code in vals.get('lots_text', '').split(', '):
-                    sim_packages = self.env['sim.package'].search([('code', '=', pkg_code)])
-                    if sim_packages and not sim_packages.partner_id:
-                        sim_packages.write({'partner_id': self.partner_id.commercial_partner_id.id,
-                                            'move_id': self.id,
-                                            'state': 'sold'})
+                sim_packages = self.env['sim.package'].search([('code', 'in', vals.get('lots_text', '').split(', '))])
+                if sim_packages and not sim_packages.partner_id:
+                    for pkg in sim_packages:
+                        pkg.write({'partner_id': self.partner_id.commercial_partner_id.id,
+                                   'move_id': self.id,
+                                   'state': 'sold'})
                         sim_packages.with_delay(priority=10).notify_sale_web('sold')
             elif self.picking_id.picking_type_id.code == 'incoming':
                 for pkg_code in vals.get('lots_text', '').split(', '):
