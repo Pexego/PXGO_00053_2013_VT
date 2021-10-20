5f7298e5bde5e13fb362641fa0b83f02cddf62e4
Albertocanal <acanal@visiotechsecurity.com>
2021-10-20 12:27:03 +0200

    [FIX] purchase_picking: eliminación de campos duplicados

--- project-addons/purchase_picking/models/stock.py
+++ project-addons/purchase_picking/models/stock.py
@@ -43,8 +43,6 @@ class StockContainer(models.Model):
     n_ref = fields.Integer(string="Nº ref", store=False, compute="_get_ref")
     forwarder = fields.Many2one('res.partner', domain="['&',('supplier','=',True),('forwarder','=',True)]",
                                 string="FWDR")
-    destination_port = fields.Many2one('stock.container.port', string='NAV/PTO')
-    status = fields.Many2one('stock.container.status', string='Status', help='For more information click on the status')
     forwarder_comercial = fields.Char(related="forwarder.comercial", store=False, string="FWDR")
     incoterm = fields.Many2one('stock.incoterms', string='Incoterm', ondelete="restrict")
     destination_port = fields.Many2one('stock.container.port', string='NAV/PTO', ondelete="restrict")
