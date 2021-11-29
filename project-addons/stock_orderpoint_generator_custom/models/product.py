148b7a1caab89b8b2448ef43a79cd5ca839033de
Albertocanal <acanal@visiotechsecurity.com>
2021-11-29 15:32:41 +0100

    [IMP] stock_orderpoint_generator_custom: Añadido nuevo módulo

---
+++ project-addons/stock_orderpoint_generator_custom/models/product.py
@@ -0,0 +1,12 @@
+from odoo import models, api
+
+class ProductProduct(models.Model):
+    _inherit = 'product.product'
+
+    @api.model
+    def create(self, vals):
+        res = super(ProductProduct, self).create(vals)
+        orderpoints = self.env['stock.warehouse.orderpoint.template'].search([('all_products','=',True),('auto_generate','=',True)])
+        orderpoints.write({'auto_product_ids':[(4,res.id)]})
+        return res
+
