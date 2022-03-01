be4e9119788dad9e5142e4d52e23b6acbe8d5ed9
Albertocanal <acanal@visiotechsecurity.com>
2022-03-01 14:55:40 +0100

    [IMP] sim_manager: Añadido envío depósito a averiados desde RMA y posibilidad de dar diferentes entradas/entregas en un RMA

--- project-addons/sim_manager/models/claim.py
+++ project-addons/sim_manager/models/claim.py
@@ -7,8 +7,9 @@ class ClaimMakePicking(models.TransientModel):
     _inherit = "claim_make_picking.wizard"
 
     @api.multi
-    def create_move(self, claim_line, p_type, picking_id, claim, note, write_field):
-        super().create_move(claim_line, p_type, picking_id, claim, note, write_field)
+    def create_move(self, wizard_line, p_type, picking_id, claim, note):
+        super().create_move(wizard_line, p_type, picking_id, claim, note)
+        claim_line = wizard_line.claim_line_id
         claim_line_id = claim_line.id
         move = picking_id.move_lines.filtered(lambda l: l.claim_line_id.id == claim_line_id)
         move.write({'lots_text': claim_line.prodlot_id})
@@ -16,7 +17,8 @@ class ClaimMakePicking(models.TransientModel):
     @api.multi
     def action_create_picking(self):
         for wizard_claim_line in self.claim_line_ids:
-            if wizard_claim_line.product_id.track_serial and not wizard_claim_line.prodlot_id:
+            claim_line = wizard_claim_line.claim_line_id
+            if claim_line.product_id.track_serial and not claim_line.prodlot_id:
                 raise UserError(_("You must specify the serial number of the serial products"))
         res = super(ClaimMakePicking, self).action_create_picking()
         return res
