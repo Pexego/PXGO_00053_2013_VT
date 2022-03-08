6292cd9008ee1aec6a15d96df0e818fd46c5faae
pmoreno-visiotech <pmoreno@visiotechsecurity.com>
2022-03-08 11:33:26 +0100

    [FIX] sale_product_customize: modificado suario origen del correo por odoo

--- project-addons/sale_product_customize/model/stock_reservation.py
+++ project-addons/sale_product_customize/model/stock_reservation.py
@@ -74,7 +74,7 @@ class StockPicking(models.Model):
                     mail_pool = self.env['mail.mail']
                     values={
                         'subject': _('Manufacturing order {} completed').format(mrp_product.name),
-                        'email_from': "Odoo-team",
+                        'email_from': "odoo_team@visiotechsecurity.com",
                         'email_to': mrp_product.user_id.login,
                         'reply_to': "",
                         'body_html': _('Your {} manufacturing order has been completed').format(mrp_product.name)
