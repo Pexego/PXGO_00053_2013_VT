bdf58ec5256802d493ae2bd606013cf05e364c4b
Albertocanal <acanal@visiotechsecurity.com>
2022-05-17 15:56:11 +0200

    [IMP] web_tree_image_custom: LImitado tamañado de la foto

---
+++ project-addons/web_tree_image_custom/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': 'Limit size tooltip images in tree views',
+    'version': '11.0.1.0.0',
+    'author': "Visiotech",
+    'license': 'AGPL-3',
+    'category': 'Web',
+    'depends': [
+        'web', 'web_tree_image'
+    ],
+    'data': [
+        'view/assets.xml',
+    ],
+    'installable': True,
+}
