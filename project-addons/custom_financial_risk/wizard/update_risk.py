3954effdb3ae9c6692806d4e688d6d3816d5f5e5
username <email>
2022-05-18 12:19:30 +0200

    [IMP] custom_financial_risk: añadido contexto para actualizar modo y plazo de pago

--- project-addons/custom_financial_risk/wizard/update_risk.py
+++ project-addons/custom_financial_risk/wizard/update_risk.py
@@ -10,32 +10,34 @@ class UpdatePartnerRisk(models.TransientModel):
 
     def action_update_risk(self):
         partners_without_team = []
+        update_payment_mode = self.env.context.get('update_payment_mode', True)
         for partner in self.partner_ids:
             partner.insurance_credit_limit = self.new_risk
             if self.new_risk == 0:
                 partner.payment_days = False
                 partner.company_credit_limit = self.new_risk
-                if partner.team_id:
-                    prepaid_id = partner.env['account.payment.term'].with_context(lang='en_US').search(
-                        [('name', '=', 'Prepaid')])
-                    partner.property_payment_term_id = prepaid_id.id
-                    if partner.team_id.name == 'Portugal':
-                        novobanco_id = partner.env['account.payment.mode'].search(
-                            [('name', '=', 'Transferencia Novo Banco')])
-                        partner.customer_payment_mode_id = novobanco_id.id
-                    if partner.team_id.name in ('Italia', 'Norte Europa', 'DACH'):
-                        caixa_id = partner.env['account.payment.mode'].search([('name', '=', 'Transferencia La Caixa')])
-                        partner.customer_payment_mode_id = caixa_id.id
-                    elif partner.team_id.name in ('Francia', 'Magreb'):
-                        popular_id = partner.env['account.payment.mode'].search(
-                            [('name', '=', 'Transferencia Popular')])
-                        partner.customer_payment_mode_id = popular_id.id
-                    elif partner.team_id.name == 'España':
-                        sabadell_id = partner.env['account.payment.mode'].search(
-                            [('name', '=', 'Transferencia Sabadell')])
-                        partner.customer_payment_mode_id = sabadell_id.id
-                else:
-                    partners_without_team.append(partner.name)
+                if update_payment_mode:
+                    if partner.team_id:
+                        prepaid_id = partner.env['account.payment.term'].with_context(lang='en_US').search(
+                            [('name', '=', 'Prepaid')])
+                        partner.property_payment_term_id = prepaid_id.id
+                        if partner.team_id.name == 'Portugal':
+                            novobanco_id = partner.env['account.payment.mode'].search(
+                                [('name', '=', 'Transferencia Novo Banco')])
+                            partner.customer_payment_mode_id = novobanco_id.id
+                        if partner.team_id.name in ('Italia', 'Norte Europa', 'DACH'):
+                            caixa_id = partner.env['account.payment.mode'].search([('name', '=', 'Transferencia La Caixa')])
+                            partner.customer_payment_mode_id = caixa_id.id
+                        elif partner.team_id.name in ('Francia', 'Magreb'):
+                            popular_id = partner.env['account.payment.mode'].search(
+                                [('name', '=', 'Transferencia Popular')])
+                            partner.customer_payment_mode_id = popular_id.id
+                        elif partner.team_id.name == 'España':
+                            sabadell_id = partner.env['account.payment.mode'].search(
+                                [('name', '=', 'Transferencia Sabadell')])
+                            partner.customer_payment_mode_id = sabadell_id.id
+                    else:
+                        partners_without_team.append(partner.name)
         if partners_without_team:
             message = _(
                 "It has not been possible to change the payment mode to transfer and the payment term to prepaid because they do not have a sales team: %s") % partners_without_team
