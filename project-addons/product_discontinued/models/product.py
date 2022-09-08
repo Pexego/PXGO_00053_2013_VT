1afeb059a74f0b26614887fcf160a17ea057bc92
Albertocanal <acanal@visiotechsecurity.com>
2022-09-08 13:17:21 +0200

    [IMP] product_discontinued: Añadido aviso a SAT de los albaranes con eol sin stock

--- project-addons/product_discontinued/models/product.py
+++ project-addons/product_discontinued/models/product.py
@@ -40,24 +40,42 @@ class ProductProduct(models.Model):
     @api.model
     def cron_send_mail_to_commercials_products_discontinued(self):
         date = (datetime.datetime.now() - datetime.timedelta(days=365)).strftime("%Y-%m-%d %H:%M:%S")
-        discontinued_products = self.env['product.product'].search([('product_tmpl_id.state', 'ilike', 'end'), ('virtual_stock_conservative', '<=', 0)])
-        moves = self.env['stock.move'].search([('state', 'in', ('partially_available', 'confirmed', 'waiting')), ('date', '>=', date),('product_id', 'in', discontinued_products.mapped('id')), ('sale_line_id', '!=', False)])
-        moves_group_by_commercial=dict()
+        discontinued_products = self.env['product.product'].search(
+            [('product_tmpl_id.state', 'ilike', 'end'), ('virtual_stock_conservative', '<=', 0)])
+        moves = self.env['stock.move'].search(
+            ['&', '&', '&', ('state', 'in', ('partially_available', 'confirmed', 'waiting')),
+             ('date', '>=', date),
+             ('product_id', 'in', discontinued_products.mapped('id')), '|',
+             ('sale_line_id', '!=', False), ('claim_line_id', '!=', False)])
+        moves_group_by_commercial = dict()
         if moves:
-            for move in moves:
-                if move.sale_line_id.salesman_id in moves_group_by_commercial:
-                    moves_group_by_commercial[move.sale_line_id.salesman_id]+=move
-                else:
-                    moves_group_by_commercial[move.sale_line_id.salesman_id] = move
-            for commercial,values in moves_group_by_commercial.items():
-                template = self.env.ref('product_discontinued.alert_cron_send_mail_to_commercials_products_discontinued')
+            moves_sales = moves.filtered(lambda m:m.sale_line_id)
+            moves_claims = moves.filtered(lambda m: m.claim_line_id)
+            template = self.env.ref('product_discontinued.alert_cron_send_mail_to_commercials_products_discontinued')
+            if moves_sales:
+                for move in moves_sales:
+                    if move.sale_line_id.salesman_id in moves_group_by_commercial:
+                        moves_group_by_commercial[move.sale_line_id.salesman_id]+=move
+                    else:
+                        moves_group_by_commercial[move.sale_line_id.salesman_id] = move
+                for commercial,values in moves_group_by_commercial.items():
+                    ctx = dict()
+                    ctx.update({
+                        'email_to': commercial.login,
+                        'moves':values,
+                        'lang':commercial.lang
+                    })
+                    template.with_context(ctx).send_mail(self.id)
+            if moves_claims:
+                support_team_user = self.env.ref('product_discontinued.support_team_user')
                 ctx = dict()
                 ctx.update({
-                    'email_to': commercial.login,
-                    'moves':values,
-                    'lang':commercial.lang
+                    'email_to': support_team_user.login,
+                    'moves': moves_claims,
+                    'lang': support_team_user.lang
                 })
                 template.with_context(ctx).send_mail(self.id)
+
 
 
 
