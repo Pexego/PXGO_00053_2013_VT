9e01153c3311a4dde1316e358d21d8ec79cccc66
Albertocanal <acanal@visiotechsecurity.com>
2022-10-13 15:52:20 +0200

    [FIX] custom_account: eliminado campo Marca de producto duplicaod en informe de ventas

--- project-addons/custom_account/report/sale_report.py
+++ project-addons/custom_account/report/sale_report.py
@@ -7,7 +7,6 @@ from odoo import models, fields
 class SaleReport(models.Model):
     _inherit = 'sale.report'
 
-    brand_id = fields.Many2one('product.brand', 'Brand')
     parent_category_id = fields.Many2one("product.category", 'Parent categ.')
     partner_ref = fields.Char("Partner ref")
     state_name = fields.Char("State Name")
@@ -17,7 +16,7 @@ class SaleReport(models.Model):
     partner_shipping_id = fields.Many2one('res.partner', string='Shipping Address')
 
     def _select(self):
-        select_str = (", t.product_brand_id as brand_id, pc.parent_id as "
+        select_str = (", pc.parent_id as "
                       "parent_category_id, rp.ref as partner_ref, rp.vat as partner_vat , cs.name as "
                       "state_name, t.manufacturer as main_supplier, s.partner_shipping_id as partner_shipping_id")
         return super()._select() + select_str
@@ -29,6 +28,6 @@ class SaleReport(models.Model):
         return super()._from() + from_str
 
     def _group_by(self):
-        group_by_str = """, t.product_brand_id, rp.ref, pc.parent_id, cs.name,
+        group_by_str = """, rp.ref, pc.parent_id, cs.name,
                           t.manufacturer, rp.vat, s.partner_shipping_id"""
         return super()._group_by() + group_by_str
