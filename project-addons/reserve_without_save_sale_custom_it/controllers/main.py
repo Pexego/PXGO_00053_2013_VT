a30da0f84979bcb8b9bfe88ebee1f414beff5524
albertocanal <acanal@visiotechsecurity.com>
2022-12-28 16:58:23 +0100

    [FIX] reserve_without_save_sale_it: Eliminar/Crear reserva en el cambio de ruta

--- project-addons/reserve_without_save_sale_custom_it/controllers/main.py
+++ project-addons/reserve_without_save_sale_custom_it/controllers/main.py
@@ -11,6 +11,11 @@ class WebsiteReservationIt(WebsiteReservation):
     @http.route(['/reservations/create'],
                 type='http', methods=['POST'], auth='user')
     def create_reservation(self, **post):
+        """
+        This inheritance helps us not to create reservations if the line is dropshipping
+        :param post: shows the info of the sale order line
+        :return True if the route is dropshipping , otherwise returns the super response
+        """
         route = post.get('route_id', False)
         if route:
             route_order = request.env.ref('stock_dropshipping.route_drop_shipping')
@@ -18,3 +23,22 @@ class WebsiteReservationIt(WebsiteReservation):
                 return json.dumps(True)
 
         return super(WebsiteReservationIt, self).create_reservation(**post)
+
+    @http.route(['/reservations/write'],
+                type='http', methods=['POST'], auth='user')
+    def write_reservation(self, **post):
+        """
+        This inheritance helps us to eliminate reservations in case the line is edited with a dropshipping route
+        :param post: shows the info of the sale order line
+        :return True if the route is dropshipping , otherwise returns the super response
+        """
+        route = post.get('route_id', False)
+        line = request.env['sale.order.line'].browse(
+            int(post['sale_line_id']))
+        if route:
+            route_order = request.env.ref('stock_dropshipping.route_drop_shipping')
+            if route_order.id == int(route):
+                line.release_stock_reservation()
+                line.write({'unique_js_id': False, 'temp_unique_js_id': False})
+                return json.dumps(True)
+        return super(WebsiteReservationIt, self).write_reservation(**post)
