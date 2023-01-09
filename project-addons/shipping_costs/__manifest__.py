a8cf3ec56e9a764961a47064258ecd612ed56a84
Alejandro Díaz Tiburón <adtiburon@visiotechsecurity.com>
2023-01-09 13:09:46 +0100

    [feature] shipping_costs: Crea el módulo shipping_costs

---
+++ project-addons/shipping_costs/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    'name': 'Shipping costs',
+    'version': '1.0',
+    'summary': '',
+    'description': '',
+    'author': 'Visiotech',
+    'depends': [],
+    'data': ['security/ir.model.access.csv'],
+    'installable': True,
+    'auto_install': False,
+}
