b5d418dd2b9be0d61240337db37d69d2f9aa5ff1
Alejandro Díaz Tiburón <adtiburon@visiotechsecurity.com>
2023-01-24 15:22:44 +0100

    [feature] shipping_costs: Modifica el comportamiento del botón del cálculo de los gastos de envío
    - Ahora incorpora el cálculo de los costes de las tarifas de los envíos por número de palets y peso.

--- project-addons/shipping_costs/__manifest__.py
+++ project-addons/shipping_costs/__manifest__.py
@@ -4,7 +4,7 @@
     'summary': '',
     'description': '',
     'author': 'Visiotech',
-    'depends': ['sale', 'transportation', 'base'],
+    'depends': ['sale', 'transportation', 'base', 'sale_order_board', 'advise_special_shipping_costs'],
     'data': ['security/ir.model.access.csv', 'views/shipping_cost_view.xml'],
     'installable': True,
     'auto_install': False,
--- project-addons/shipping_costs/models/sale_order.py
+++ project-addons/shipping_costs/models/sale_order.py
@@ -1,7 +1,110 @@
-from odoo import models, fields
+from odoo import models, fields, api
 
 
 class SaleOrder(models.Model):
     _inherit = "sale.order"
 
-    # TODO: más tarde se usará para relacionar con la tarifa de coste
+    sale_order_shipping_cost_id = fields.One2many(
+        "sale.order.shipping.cost",
+        "sale_order_id",
+        "Shipping Cost"
+    )
+
+    @api.multi
+    def compute_variables(self):
+        """
+        Overrides compute_variables in sale_order_board module
+        Calculates the shipping costs.
+        When we have special shipping costs, calculates pallet shipping costs.
+        If we don't have special shipping costs, calculates all shipping costs.
+        """
+        picking_rated = self.env['picking.rated.wizard'].create({})
+        pallet_services_to_add = []
+        available_shipping_costs = self._get_available_shipping_costs()
+        # calculate pallet shipping costs
+        for shipping_cost in available_shipping_costs:
+            new_so_sc = self.env['sale.order.shipping.cost'].create({
+                'sale_order_id': self.id,
+                'shipping_cost_id': shipping_cost.id
+            })
+
+            pallet_service_cost_list = new_so_sc.calculate_shipping_cost()
+            # FIXME: assign correctly currency and transit_time
+            pallet_services_to_add = [
+                (0, 0, {
+                    'currency': shipping_cost.cost_name,
+                    'transit_time': 'Por pallet',
+                    'amount': service['price'],
+                    'service': service['service_name'],
+                    'order_id': self.id,
+                    'wizard_id': picking_rated.id
+                }) for service in pallet_service_cost_list
+            ]
+        # if we have special shipping costs we only need pallet service costs
+        if self.is_special_shipping_costs:
+            picking_rated.write({'data': pallet_services_to_add})
+            return {
+                'name': 'Shipping Data Information',
+                'view_type': 'form',
+                'view_mode': 'form',
+                'target': 'new',
+                'res_model': 'picking.rated.wizard',
+                'src_model': 'stock.picking',
+                'res_id': picking_rated.id,
+                'type': 'ir.actions.act_window',
+                'id': 'action_picking_rated_status',
+            }
+
+        response = super().compute_variables()
+        new_id = response['res_id']
+        picking_rated = self.env['picking.rated.wizard'].browse(new_id)
+        picking_rated.write({'data': pallet_services_to_add})
+
+        # we get the rest service costs
+        for shipping_cost in available_shipping_costs:
+            new_so_sc = self.env['sale.order.shipping.cost'].create({
+                'sale_order_id': self.id,
+                'shipping_cost_id': shipping_cost.id
+            })
+            for service in new_so_sc.calculate_shipping_cost(pallet_mode=False):
+                # FIXME: assign correctly currency and transit_time
+                rated_status = {
+                    'currency': shipping_cost.cost_name,
+                    'transit_time': 'Por peso',
+                    'amount': service['price'],
+                    'service': service['service_name'],
+                    'order_id': self.id,
+                    'wizard_id': picking_rated.id
+                }
+                picking_rated.write({'data': [(0, 0, rated_status)]})
+
+        return {
+            'name': 'Shipping Data Information',
+            'view_type': 'form',
+            'view_mode': 'form',
+            'target': 'new',
+            'res_model': 'picking.rated.wizard',
+            'src_model': 'stock.picking',
+            'res_id': picking_rated.id,
+            'type': 'ir.actions.act_window',
+            'id': 'action_picking_rated_status',
+        }
+
+    def _get_available_shipping_costs(self):
+        """
+        Returns a list of all shipping_cost that fit with the sale_order
+        """
+        shipping_cost_list = self.env['shipping.cost'].search([])
+        shipping_cost_available = []
+        # we get shipping_cost that verifies shipping_conditions - empty conditions are included
+        for shipping_cost in shipping_cost_list:
+            checker_list = [
+                eval(
+                    f'"{getattr(self, condition.filter_by).name}" {condition.operator} "{condition.arguments}"'
+                ) if self._fields[condition.filter_by].type in ('many2one', 'one2many', 'many2many') else eval(
+                    f'"{getattr(self, condition.filter_by)}" {condition.operator} "{condition.arguments}"'
+                ) for condition in shipping_cost.condition_ids
+            ]
+            if all(checker_list):
+                shipping_cost_available.append(shipping_cost)
+        return shipping_cost_available
