6dabfa85b03d1cb6e01fd6cc658beb5d9907b6f4
Alejandro Díaz Tiburón <adtiburon@visiotechsecurity.com>
2023-02-08 10:11:44 +0100

    [fix] purchase_picking: Corrige el cálculo del número de hojas de importación en envíos

--- project-addons/purchase_picking/models/stock.py
+++ project-addons/purchase_picking/models/stock.py
@@ -185,7 +185,8 @@ class StockContainer(models.Model):
         Gets the list of import_sheet associated with the container and calculates
         the count of them.
         """
-        self.import_sheet_count = len(self.import_sheet_ids)
+        for container in self:
+            container.import_sheet_count = len(container.import_sheet_ids)
 
     def action_view_sheets(self):
 
