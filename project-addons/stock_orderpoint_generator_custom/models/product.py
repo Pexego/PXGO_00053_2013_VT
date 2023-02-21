5fd50837fd33e4ee9e10176521351c3ac2f1b280
albertocanal <acanal@visiotechsecurity.com>
2023-02-21 13:30:24 +0100

    [FIX] stock_orderpoint_generator_custom: añadir/eliminar a la plantilla de generacióna utomática dependiendo del tipo y añadido comprobación al crearse el product.template

--- project-addons/stock_orderpoint_generator_custom/models/product.py
+++ project-addons/stock_orderpoint_generator_custom/models/product.py
@@ -6,9 +6,8 @@ class ProductProduct(models.Model):
     @api.model
     def create(self, vals):
         res = super(ProductProduct, self).create(vals)
-        if 'type' in vals and vals['type']=='product':
-            orderpoints = self.env['stock.warehouse.orderpoint.template'].search([('all_products','=',True),('auto_generate','=',True)])
-            orderpoints.write({'auto_product_ids':[(4,res.id)]})
+        if 'type' in vals and vals['type'] == 'product':
+            res.add_products_to_orderpoint_template()
         return res
 
     @api.multi
@@ -30,14 +29,59 @@ class ProductProduct(models.Model):
         if res and self._context.get('modify_stock_qty', False):
             for product_id in res.keys():
                 product = self.env['product.product'].browse(product_id)
-                product_stock= res[product_id]
+                product_stock = res[product_id]
                 w_res_count = product.waiting_reservation_count
                 res[product_id]['outgoing_qty'] -= w_res_count
                 moves_to_location = self.env['stock.move'].search(
-                    [('product_id', '=', product_id), ('state', 'not in', ['draft','done', 'cancel']),
+                    [('product_id', '=', product_id), ('state', 'not in', ['draft', 'done', 'cancel']),
                      ('location_dest_id', '=', self._context.get('location')),
                      ('location_id', '=', self.env.ref('automatize_edi_it.stock_location_vendor_deposit').id)])
                 res[product_id]['incoming_qty'] = sum(moves_to_location.mapped('product_qty'))
-                res[product_id]['virtual_available'] = product_stock['qty_available'] + product_stock['incoming_qty']\
+                res[product_id]['virtual_available'] = product_stock['qty_available'] + product_stock['incoming_qty'] \
                                                        - product_stock['outgoing_qty']
         return res
+
+    @api.multi
+    def add_products_to_orderpoint_template(self):
+        orderpoints = self.env['stock.warehouse.orderpoint.template'].search(
+            [('all_products', '=', True), ('auto_generate', '=', True)])
+        for product in self:
+            orderpoints.write({'auto_product_ids': [(4, product.id)]})
+
+    @api.multi
+    def remove_products_to_orderpoint_template(self):
+        orderpoints = self.env['stock.warehouse.orderpoint.template'].search(
+            [('all_products', '=', True), ('auto_generate', '=', True)])
+        for product in self:
+            orderpoints.write({'auto_product_ids': [(3, product.id)]})
+
+    @api.multi
+    def write(self, vals):
+        for product in self:
+            p_type = vals.get('type', '')
+            if p_type == 'product' and product.type != 'product':
+                product.add_products_to_orderpoint_template()
+            elif p_type != 'product' and product.type == 'product':
+                product.remove_products_to_orderpoint_template()
+        return super(ProductProduct, self).write(vals)
+
+
+class ProductTemplate(models.Model):
+    _inherit = 'product.template'
+
+    @api.model
+    def create(self, vals):
+        res = super(ProductTemplate, self).create(vals)
+        if 'type' in vals and vals['type'] == 'product':
+            res.product_variant_ids.add_products_to_orderpoint_template()
+        return res
+
+    @api.multi
+    def write(self, vals):
+        for product in self:
+            p_type = vals.get('type', '')
+            if p_type == 'product' and product.type != 'product':
+                product.product_variant_ids.add_products_to_orderpoint_template()
+            elif p_type != 'product' and product.type == 'product':
+                product.product_variant_ids.remove_products_to_orderpoint_template()
+        return super(ProductTemplate, self).write(vals)
