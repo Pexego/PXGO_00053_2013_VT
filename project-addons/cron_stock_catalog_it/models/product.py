8ccd5861cd3d44cc8045c4467cad30ec22412c1e
albertocanal <acanal@visiotechsecurity.com>
2023-02-28 12:36:56 +0100

    [REF] cron_Stock_catalog_it: quitados imports innecesarios y añadido docstring

--- project-addons/cron_stock_catalog_it/models/product.py
+++ project-addons/cron_stock_catalog_it/models/product.py
@@ -1,12 +1,10 @@
-from odoo import api, fields, models, _, exceptions, tools
-import base64
-from datetime import datetime,timedelta
-import xlsxwriter
+from odoo import models
 
 class ProductProduct(models.Model):
     _inherit = 'product.product'
 
     def _get_eol_stock_move_domain(self, date):
+        """ :returns the super domain adding not dropship picking """
         domain = super()._get_eol_stock_move_domain(date)
         dropship_route = self.env.ref('stock_dropshipping.picking_type_dropship')
         return domain + [('picking_id.picking_type_id', '!=', dropship_route.id)]
