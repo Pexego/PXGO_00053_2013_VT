663cf1ef4b5d9ca7d49ac9cf67e8e7cace5e54cc
albertocanal <acanal@visiotechsecurity.com>
2023-03-07 15:47:33 +0100

    [FIX] cron_stock_catalog_it: corregido para que funcione en ambos crons

--- project-addons/cron_stock_catalog_it/models/product.py
+++ project-addons/cron_stock_catalog_it/models/product.py
@@ -8,4 +8,4 @@ class ProductProduct(models.Model):
         """ :returns the super domain adding not dropship picking """
         domain = super()._get_eol_stock_move_domain(date)
         dropship_route = self.env.ref('stock_dropshipping.picking_type_dropship')
-        return domain + [('picking_id.picking_type_id', '!=', dropship_route.id)]
+        return  ['&',('picking_id.picking_type_id', '!=', dropship_route.id)] + domain
