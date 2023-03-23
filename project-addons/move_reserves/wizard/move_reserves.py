4216b6cc0562704aef3fed04b691b03e0ecd7653
rcerrillo2 <rcerrillo@visiotechsecurity.com>
2023-03-23 17:15:21 +0100

    [IMP]move_reserves: nuevo módulo para mover reservas de un movimiento a otro

---
+++ project-addons/move_reserves/wizard/move_reserves.py
@@ -0,0 +1,99 @@
+from odoo import fields, models, api, _
+from odoo.exceptions import UserError
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
+
+
+class MoveReserves(models.TransientModel):
+    _name = "move.reserves"
+
+    product_id = fields.Many2one("product.product", "Product")
+    qty = fields.Integer("Quantity")
+
+    reserves_origin_id = fields.Many2one("stock.move")  # TODO: filtrar esto por usuario, para que no puedan robar
+    reserves_dest_id = fields.Many2one("stock.move")
+
+    @api.onchange('product_id', 'qty')
+    def on_change_product_id_origin(self):
+        """Changes the domain of the reserves_origin_id field"""
+        if self.product_id:
+            return {'domain': {'reserves_origin_id': [('product_id', '=', self.product_id.id),
+                                                      ('picking_type_id.code', '=', 'outgoing'),
+                                                      ('state', 'not in', ['done', 'draft', 'cancel'])]}}
+
+    @api.onchange('product_id', 'qty')
+    def on_change_product_id_dest(self):
+        """Changes the domain of the reserves_dest_id field"""
+        if self.product_id:
+            return {'domain': {'reserves_dest_id': [('product_id', '=', self.product_id.id),
+                                                    ('state', 'not in', ['done', 'draft', 'cancel', 'assigned'])]}}
+
+    def create_dummy_reserve(self, product, qty):
+        """Creates a dummy reserve that serves as an intermediate"""
+        now = datetime.now()
+        date_validity = (now + relativedelta(days=1)).strftime("%Y-%m-%d")
+        warehouse = self.env['stock.warehouse'].browse(1)
+        vals = {
+            'product_id': product.id,
+            'product_uom': 1,
+            'product_uom_qty': qty,
+            'date_validity': date_validity,
+            'name': product.name,
+            'location_id': warehouse.lot_stock_id.id,
+            'user_id': self.env.user.id
+        }
+        new_reservation = self.env['stock.reservation'].create(vals)
+        return new_reservation
+
+    def action_move_reserve(self):
+        """
+        Move reserves from one move to another
+        Process:
+            1. create reserve intermediate
+            2. release origin
+            3. reserve intermediate
+            4. reserve origin
+            5. release intermediate
+            6. reserve destination
+            7. delete intermediate
+        """
+        if self.reserves_origin_id.id == self.reserves_dest_id.id:
+            raise UserError(_('You have selected the same move in both'))
+        if self.qty > self.reserves_origin_id.reserved_availability:
+            raise UserError(_('You have selected more quantity than is available in the origin reserve'))
+        if self.qty > self.reserves_dest_id.product_uom_qty:
+            raise UserError(_('You have selected more quantity than is in the destination reserve'))
+
+        dummy_reserve = self.create_dummy_reserve(self.product_id, self.qty)
+        origin_qty_reserved = self.reserves_origin_id.reserved_availability
+
+        try:
+            if not self.reserves_origin_id.reservation_ids:
+                # When is reserved directly on the picking there is not stock.reservation
+                self.reserves_origin_id.action_do_unreserve()
+            else:
+                self.reserves_origin_id.reservation_ids.release()  # TODO: sudo??
+            dummy_reserve.reserve()
+            if self.qty != origin_qty_reserved:
+                self.reserves_origin_id.reservation_ids.reserve()
+            dummy_reserve.release()
+            self.reserves_dest_id._action_assign()
+            dummy_reserve.unlink()
+        except:
+            raise UserError(_('There is been an error, try again later'))
+
+
+class StockMove(models.Model):
+    _inherit = "stock.move"
+
+    @api.multi
+    @api.depends('origin', 'user_id', 'string_availability_info')
+    def name_get(self):
+        result = []
+        for move in self:
+            if move.picking_id:
+                name = (str(int(move.reserved_availability)) or '_') + ' uds' + ' | ' + (move.origin or '_') + ' | ' + (move.picking_id.name or '_') + ' | ' + (move.user_id.name or '_')
+            else:
+                name = (str(int(move.reserved_availability)) or '_') + ' uds' + ' | ' + (move.origin or '_') + ' | ' + (move.user_id.name or '_')
+            result.append((move.id, name))
+        return result
