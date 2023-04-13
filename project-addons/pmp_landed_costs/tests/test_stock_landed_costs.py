d1d1fd6cf210ffd2c77ee83e2b4badef80b47044
Alejandro Díaz Tiburón <adtiburon@visiotechsecurity.com>
2023-04-13 09:52:03 +0200

    [IMP] pmp_landed_costs: Añande tests para landed_cost_creator

--- project-addons/pmp_landed_costs/tests/test_stock_landed_costs.py
+++ project-addons/pmp_landed_costs/tests/test_stock_landed_costs.py
@@ -55,3 +55,40 @@ class TestStockLandedCost(SavepointCase):
             self.stock_landed_cost.cost_lines
         )
         self.assertEqual(expected_value, obtained_value)
+
+
+class TestLandedCostCreator(SavepointCase):
+    post_install = True
+    at_install = True
+
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.container = cls.env['stock.container'].create({
+            'name': 'Test container',
+            'type': 'air'
+        })
+        cls.import_sheet = cls.env['import.sheet'].create({
+            'container_id': cls.container.id,
+            'dua': 'Test Dua'
+        })
+        cls.landed_cost_creator = cls.env['landed.cost.creator.wizard'].create({
+            'import_sheet_id': cls.import_sheet.id
+        })
+
+    def test_create_landed_cost(self):
+        start_cost_count_expected = 0
+        start_cost_count_obtained = self.import_sheet.landed_cost_count
+
+        self.landed_cost_creator.create_landed_cost()
+        # recalculate landed_cost_count value, else got from cache and equals to 0
+        self.import_sheet._get_landed_cost_count()
+
+        end_cost_count_expected = 1
+        end_cost_count_obtained = self.import_sheet.landed_cost_count
+        cost_line_count_expected = 2
+        cost_line_count_obtained = len(self.import_sheet.landed_cost_ids.cost_lines)
+
+        self.assertEquals(start_cost_count_expected, start_cost_count_obtained)
+        self.assertEquals(end_cost_count_expected, end_cost_count_obtained)
+        self.assertEquals(cost_line_count_expected, cost_line_count_obtained)
