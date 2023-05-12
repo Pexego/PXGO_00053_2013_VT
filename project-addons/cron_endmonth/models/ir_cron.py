a16444556d03fbc129a6586fab53508669cd5cac
rcerrillo2 <rcerrillo@visiotechsecurity.com>
2023-05-12 08:35:12 +0200

    [IMP]cron_end_month: se añade una nueva función para final de mes no laborable

--- project-addons/cron_endmonth/models/ir_cron.py
+++ project-addons/cron_endmonth/models/ir_cron.py
@@ -5,7 +5,7 @@ from dateutil.relativedelta import relativedelta
 import calendar
 
 
-def get_delta_end_month():
+def get_delta_end_month_w():
     next_month = (datetime.now() + relativedelta(months=1)).month
     next_year = datetime.now().year
     if next_month == 1:
@@ -16,13 +16,27 @@ def get_delta_end_month():
 
     if next_date.weekday() in (5, 6):  # Saturday or Sunday
         # Having 5 (saturday) -4 will be 1 day less (friday) and 6-4 will be 2 days less
-        days_to_decrease = next_date.weekday() - 4  
+        days_to_decrease = next_date.weekday() - 4
         next_date = datetime.datetime(next_year, next_month, next_day - days_to_decrease)
 
     difference = next_date.date() - datetime.today().date()
     return relativedelta(days=difference.days)
 
 
+def get_delta_end_month():
+    next_month = (datetime.now() + relativedelta(months=1)).month
+    next_year = datetime.now().year
+    if next_month == 1:
+        next_year += 1
+
+    next_day = calendar.monthrange(next_year, next_month)[1]
+    next_date = datetime(next_year, next_month, next_day)
+
+    difference = next_date.date() - datetime.today().date()
+    return relativedelta(days=difference.days)
+
+
+_intervalTypes['end_month_w'] = lambda interval: get_delta_end_month_w()
 _intervalTypes['end_month'] = lambda interval: get_delta_end_month()
 
 
@@ -30,4 +44,4 @@ class ir_cron(models.Model):
 
     _inherit = "ir.cron"
 
-    interval_type = fields.Selection(selection_add=[('end_month', 'End of month')])
+    interval_type = fields.Selection(selection_add=[('end_month_w', 'End of month Workday'), ('end_month', 'End of month')])
