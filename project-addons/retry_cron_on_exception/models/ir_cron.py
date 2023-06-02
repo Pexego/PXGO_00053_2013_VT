1f7d73bae29237c791ce0caf9ab3065e13059eec
albertocanal <acanal@visiotechsecurity.com>
2023-06-02 10:21:41 +0200

    [IMP] retry_cron_on_exceptions: añadido límite de reintentos

--- project-addons/retry_cron_on_exception/models/ir_cron.py
+++ project-addons/retry_cron_on_exception/models/ir_cron.py
@@ -1,5 +1,4 @@
 from odoo import api, fields, models
-from datetime import datetime
 from dateutil.relativedelta import relativedelta
 from psycopg2.extensions import TransactionRollbackError
 from odoo.addons.retry_cron_on_exception.models.exceptions import RetryCallbackException
@@ -8,7 +7,14 @@ class IrCron(models.Model):
     _inherit = "ir.cron"
 
     retry_on_exception = fields.Boolean(help="If This field is checked, the cron will be retried if it fails")
-    retry_on_exception_time = fields.Integer(default=5, help="This field represents the time in minutes that the cron will be retried if it fails")
+    retry_on_exception_time = fields.Integer(default=0, help="This field represents the time in minutes that the cron will be retried if it fails")
+    current_retry = fields.Integer(string='Current try', default=0, help="The current retry of this cron")
+    max_retries = fields.Integer(
+        string='Max. retries',
+        help="The cron will fail if the number of tries reach the "
+             "max. retries.\n"
+             "Retries are infinite when 0.",
+    )
 
     @classmethod
     def _process_job(cls, job_cr, job, cron_cr):
@@ -18,10 +24,15 @@ class IrCron(models.Model):
             :param cron_cr: cursor holding lock on the cron job row, to use to update the next exec date,
                    must not be committed/rolled back!
         """
+        nextcall = fields.Datetime.from_string(job['nextcall'])
         try:
             super()._process_job(job_cr,job,cron_cr)
+            cron_cr.execute(
+                f"UPDATE ir_cron SET current_retry=0 WHERE id={job['id']}")
+            cron_cr.commit()
         except RetryCallbackException as e:
-            cron_cr.execute(f"UPDATE ir_cron SET nextcall='{datetime.now() + relativedelta(minutes=e.time)}' WHERE id={job['id']}")
+            cron_cr.execute(
+                f"UPDATE ir_cron SET nextcall='{nextcall + relativedelta(minutes=e.time)}', current_retry={job['current_retry'] + 1} WHERE id={job['id']}")
             cron_cr.commit()
 
     @api.model
@@ -29,7 +40,8 @@ class IrCron(models.Model):
         """
             The original function is inherited to add the possibility of retrying a cron if an exception has occurred.
             If the cron has checked the field retry_on_exception, and it is a concurrency exception we throw an
-            RetryCallbackException that will be caught in another function in order to retry the cron
+            RetryCallbackException (only if the current_retry < max retires or max_retries==0)
+            that will be caught in another function in order to retry the cron
         :param cron_name: Name of the cron (string)
         :param server_action_id: Id of the action executed
         :param job_id: Id of the ir.cron
@@ -40,5 +52,6 @@ class IrCron(models.Model):
                                                        job_id,
                                                        job_exception)
         my_cron = self.env['ir.cron'].browse(job_id)
-        if my_cron.retry_on_exception and isinstance(job_exception, TransactionRollbackError):
-            raise RetryCallbackException(job_exception,my_cron.retry_on_exception_time)
+        if my_cron.retry_on_exception and (not my_cron.max_retries or my_cron.current_retry < my_cron.max_retries) \
+            and isinstance(job_exception, TransactionRollbackError):
+            raise RetryCallbackException(job_exception, my_cron.retry_on_exception_time)
