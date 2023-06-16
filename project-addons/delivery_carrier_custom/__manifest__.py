c8becc5936d2c0a77599eeeecfb682eb43f28d4a
Alejandro Díaz Tiburón <adtiburon@visiotechsecurity.com>
2023-06-16 09:20:40 +0200

    [IMP] delivery_carrier_custom: Crea el módulo delivery_carrier_custom

---
+++ project-addons/delivery_carrier_custom/__manifest__.py
@@ -0,0 +1,10 @@
+{
+    'name': 'Delivery Carrier Custom',
+    'version': '1.0',
+    'category': 'Customization',
+    'description': 'Customizes Delivery Carrier',
+    'author': 'Visiotech',
+    'depends': ['delivery_carrier_partner'],
+    'data': [],
+    'installable': True
+}
