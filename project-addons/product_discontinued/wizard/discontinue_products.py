fcba054f99bb99e792543f1e4b846abc6c034eaf
Alejandro Díaz Tiburón <adtiburon@visiotechsecurity.com>
2023-07-12 13:35:46 +0200

    [IMP] product_discontinued: Cambios de estilo

--- project-addons/product_discontinued/wizard/discontinue_products.py
+++ project-addons/product_discontinued/wizard/discontinue_products.py
@@ -1,21 +1,20 @@
 from odoo import models, fields, api, exceptions, _
 
+
 class CreatePickingMove(models.TransientModel):
 
     _name = 'discontinue.products.wizard'
 
-
     @api.model
     def _get_products(self):
         product_ids = self.env.context.get('active_ids', [])
-        products = self.env['product.product'].search([('id','in',product_ids)])
+        products = self.env['product.product'].search([('id', 'in', product_ids)])
         wiz_lines = []
         for product in products:
             wiz_lines.append({'product_id': product})
         return wiz_lines
 
     product_ids = fields.One2many('discontinue.product.info.wizard', 'wizard_id', default=_get_products)
-
 
     @api.multi
     def action_discontinue_products(self):
@@ -25,10 +24,11 @@ class CreatePickingMove(models.TransientModel):
         for product in self.env['product.product'].browse(context.get('active_ids')):
             try:
                 product.write({'discontinued': True})
-            except Exception as ex :
-                message = product.default_code +" --> " + str(ex)[2:-8]
-                self.env.user.notify_warning(message = message, sticky=True)
+            except Exception as ex:
+                message = product.default_code + " --> " + str(ex)[2:-8]
+                self.env.user.notify_warning(message=message, sticky=True)
                 pass
+
 
 class ProductProductWizard(models.TransientModel):
 
@@ -37,4 +37,3 @@ class ProductProductWizard(models.TransientModel):
     wizard_id = fields.Many2one('discontinue.products.wizard', 'wizard')
 
     product_id = fields.Many2one('product.product', 'Product')
-
