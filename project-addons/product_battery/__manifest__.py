fca28954613b8fb4d4b6100e6436bce42b79883a
Alejandro Díaz Tiburón <adtiburon@visiotechsecurity.com>
2023-07-13 14:34:25 +0200

    [IMP] product_battery: Elimina dependencia de transportation

--- project-addons/product_battery/__manifest__.py
+++ project-addons/product_battery/__manifest__.py
@@ -5,7 +5,7 @@
     'description': """""",
     'author': 'Visiotech',
     'website': 'www.visiotechsecurity.com',
-    "depends": ['product', 'stock', 'transportation'],
+    "depends": ['product', 'stock'],
     "data": ['data/battery_data.xml', 'views/product_view.xml', 'views/battery_view.xml', 'views/sale_view.xml',
              'security/ir.model.access.csv'
              ],
