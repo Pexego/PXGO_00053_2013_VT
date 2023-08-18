97226b69f93a2f161894adaa4abe05f7bf826600
albertocanal <acanal@visiotechsecurity.com>
2023-08-18 09:22:01 +0200

    [IMP] separate_purchase_orders: abrir vista de OC en wizard stock fabricando

--- project-addons/separate_purchase_orders/wizard/view_stock.py
+++ project-addons/separate_purchase_orders/wizard/view_stock.py
@@ -23,4 +23,30 @@ class ViewStockWizard(models.TransientModel):
 
         return res
 
+class ViewStockLines(models.TransientModel):
+    _inherit = 'view.stock.lines'
+
+
+    def _show_separate_purchase_order(self):
+        """
+        This method displays the view of OC associated with the line
+        :return: action
+        """
+        action = self.env.ref('separate_purchase_orders.action_open_purchase_orders_in_purchase_order_state')
+        result = action.read()[0]
+        res = self.env.ref('separate_purchase_orders.purchase_order_form_custom', False)
+        result['views'] = [(res and res.id or False, 'form')]
+        result['res_id'] = self.purchase_id.id
+        return result
+
+    def open_element(self):
+        """
+        This method displays the view of the element associated with the line
+        :return: action
+        """
+
+        if self.purchase_id and self.purchase_id.state=='purchase_order':
+            return self._show_separate_purchase_order()
+        return super().open_element()
+
 
