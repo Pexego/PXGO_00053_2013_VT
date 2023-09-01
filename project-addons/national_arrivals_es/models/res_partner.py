4dffdd9181c15b883cff6b8b066e583c73aa202b
Alejandro Díaz Tiburón <adtiburon@visiotechsecurity.com>
2023-09-01 10:33:24 +0200

    [IMP] national_arrivals_es: Añade el campo para ver si es proveedor nacional
    - Con esto se reduce el tiempo de carga de la vista de Llegadas nacionales

---
+++ project-addons/national_arrivals_es/models/res_partner.py
@@ -0,0 +1,23 @@
+from odoo import models, api, fields
+
+
+class ResPartner(models.Model):
+    _inherit = "res.partner"
+
+    is_national_supplier = fields.Boolean(
+        'Is national supplier',
+        compute='_compute_is_national_supplier',
+        store=True
+    )
+
+    def _compute_is_national_supplier(self):
+        """
+        Calculates if a supplier has intra or national fiscal position
+        """
+        national_fiscal_position_id = self.env.ref('l10n_es.1_fp_nacional').id
+        intra_fiscal_position_id = self.env.ref('l10n_es.1_fp_intra').id
+        for partner in self:
+            partner.is_national_supplier = partner.supplier and partner.property_account_position_id.id in (
+                national_fiscal_position_id,
+                intra_fiscal_position_id
+            )
